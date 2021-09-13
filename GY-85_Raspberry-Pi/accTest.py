d0c251bd0b15c5e7e90e15e41b1b2944c894f6fe
helixing <helixing99@gmail.com>
2021-09-13 20:11:34 +0800

    1

--- GY-85_Raspberry-Pi/accTest.py
+++ GY-85_Raspberry-Pi/accTest.py
@@ -3,10 +3,13 @@ sys.path.insert(1,'/home/pi/audioproject/GY-85_Raspberry-Pi/i2clibraries')
 from i2c_adxl345 import *
 from time import *
 adxl345 = i2c_adxl345(1)
+a = 0
+time_start = time()
 with open("acc.txt", "w") as writer:
     while True:
         if adxl345.getInterruptStatus():
             (x, y, z) = adxl345.getAxes()
-            data = [str(x), str(y), str(z), str(time())]
-            writer.write(" ".join(data) + '\n')
+            print((time() - time_start)/a)
+            #data = [str(x), str(y), str(z), str(time())]
+            # writer.write(" ".join(data) + '\n')
 
