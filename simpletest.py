35fa2690657f772c6f26a2b4ba3cf0daf6d73c7a
helixing <helixing99@gmail.com>
2021-09-13 21:41:05 +0800

    1

--- simpletest.py
+++ simpletest.py
@@ -6,9 +6,9 @@ import adafruit_adxl34x
 i2c = board.I2C()
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 a = 0
-accelerometer.data_rate(adafruit_adxl34x.DataRate.RATE_3200_HZ)
+accelerometer._write_register_byte(adafruit_adxl34x.DataRate.RATE_3200_HZ)
 time_start = time.time()
 while True:
     a = a + 1
     print((time.time() - time_start) / a)
-    print("%f %f %f"%accelerometer.acceleration)
+    print("%f %f %f" % accelerometer.acceleration)
