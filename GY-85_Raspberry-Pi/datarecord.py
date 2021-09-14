fbca5ca18db297e3eb90b33099a298013b4a06f5
helixing <helixing99@gmail.com>
2021-09-14 15:59:19 +0800

    1

--- GY-85_Raspberry-Pi/datarecord.py
+++ GY-85_Raspberry-Pi/datarecord.py
@@ -4,7 +4,7 @@ from i2c_adxl345 import *
 from i2c_itg3205 import *
 from i2c_hmc5883l import *
 import time
-a = 0
+a, b, c = 0, 0, 0
 adxl345 = i2c_adxl345(1)
 hmc5883l = i2c_hmc5883l(1)
 itg3205 = i2c_itg3205(1)
@@ -13,23 +13,29 @@ hmc5883l.setDeclination(3, 5)
 accwriter = open('acc.txt', 'w')
 gyrowriter = open('gyro.txt', 'w')
 compasswriter = open('compass.txt', 'w')
-time_start = time()
-while True:
-    a = a + 1
+time_start = time.time()
+for i in range(10000):
     if ( a % 200 == 0):
         (x, y, z) = hmc5883l.getAxes()
-        gyrowriter.write(str(x) + ' ' + str(y) + ' ' + str(z) + '\n')
+        compasswriter.write(str(x) + ' ' + str(y) + ' ' + str(z) + '\n')
+        c = c + 1
     itgready, dataready = itg3205.getInterruptStatus()
     if adxl345.getInterruptStatus() & dataready:
         (x1, y1, z1) = adxl345.getRawAxes()
         (x2, y2, z2) = itg3205.getDegPerSecAxes()
         accwriter.write(str(x1) + ' ' + str(y1) + ' ' + str(z1) + '\n')
         gyrowriter.write(str(x2) + ' ' + str(y2) + ' ' + str(z2) + '\n')
+        a = a + 1
+        b = b + 1
     elif adxl345.getInterruptStatus():
         (x1, y1, z1) = adxl345.getRawAxes()
         accwriter.write(str(x1) + ' ' + str(y1) + ' ' + str(z1) + '\n')
+        a = a + 1
     elif dataready:
         (x2, y2, z2) = itg3205.getDegPerSecAxes()
         gyrowriter.write(str(x2) + ' ' + str(y2) + ' ' + str(z2) + '\n')
+        b = b + 1
     else:
         pass
+time_pass = time.time() - time_start
+print(a/time_pass, b/time_pass, c/time_pass)
