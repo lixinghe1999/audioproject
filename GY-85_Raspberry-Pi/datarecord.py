39b77a018f4b92834ca3e2e8cfbfedceec6a0da8
helixing <helixing99@gmail.com>
2021-09-14 16:19:46 +0800

    1

--- GY-85_Raspberry-Pi/datarecord.py
+++ GY-85_Raspberry-Pi/datarecord.py
@@ -10,8 +10,6 @@ import time
 hmc5883l = i2c_hmc5883l(1)
 hmc5883l.setContinuousMode()
 hmc5883l.setDeclination(3, 5)
-
-
 compasswriter = open('compass.txt', 'w')
 time_start = time.time()
 def acc_save(time_start):
@@ -38,8 +36,8 @@ def gyro_save(time_start):
             b = b + 1
     print('gyro 1000')
     print((time.time() - time_start) / b)
-thread1 = threading.Thread(target = acc_save, args = (time_start))
-thread2 = threading.Thread(target = gyro_save, args =(time_start))
+thread1 = threading.Thread(target = acc_save, args = (time_start,))
+thread2 = threading.Thread(target = gyro_save, args =(time_start,))
 thread1.start()
 thread2.start()
 thread1.join()
