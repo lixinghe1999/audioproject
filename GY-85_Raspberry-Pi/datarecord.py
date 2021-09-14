83606a85bc08a9187d75d8ba967acf4c85f8f4b0
helixing <helixing99@gmail.com>
2021-09-14 15:55:45 +0800

    1

--- GY-85_Raspberry-Pi/datarecord.py
+++ GY-85_Raspberry-Pi/datarecord.py
@@ -3,28 +3,33 @@ sys.path.insert(1,'/home/pi/audioproject/GY-85_Raspberry-Pi/i2clibraries')
 from i2c_adxl345 import *
 from i2c_itg3205 import *
 from i2c_hmc5883l import *
-from time import *
-
+import time
 a = 0
 adxl345 = i2c_adxl345(1)
 hmc5883l = i2c_hmc5883l(1)
 itg3205 = i2c_itg3205(1)
 hmc5883l.setContinuousMode()
 hmc5883l.setDeclination(3, 5)
-
+accwriter = open('acc.txt', 'w')
+gyrowriter = open('gyro.txt', 'w')
+compasswriter = open('compass.txt', 'w')
 time_start = time()
 while True:
     a = a + 1
-    print((time()-time_start) / a)
-    # save acc data
-    adxl345.save()
-    print(hmc5883l)
-    (itgready, dataready) = itg3205.getInterruptStatus()
-    if dataready:
-        temp = itg3205.getDieTemperature()
-        (x, y, z) = itg3205.getDegPerSecAxes()
-        # print ("Temp:" + str (temp ))
-        # print ("X:" + str (x ))
-        # print ("Y:" + str (y ))
-        # print ("Z:" + str (z ))
-        # print ("")
+    if ( a % 200 == 0):
+        (x, y, z) = hmc5883l.getAxes()
+        gyrowriter.write(str(x) + ' ' + str(y) + ' ' + str(z) + '\n')
+    itgready, dataready = itg3205.getInterruptStatus()
+    if adxl345.getInterruptStatus() & dataready:
+        (x1, y1, z1) = adxl345.getRawAxes()
+        (x2, y2, z2) = itg3205.getDegPerSecAxes()
+        accwriter.write(str(x1) + ' ' + str(y1) + ' ' + str(z1) + '\n')
+        gyrowriter.write(str(x2) + ' ' + str(y2) + ' ' + str(z2) + '\n')
+    elif adxl345.getInterruptStatus():
+        (x1, y1, z1) = adxl345.getRawAxes()
+        accwriter.write(str(x1) + ' ' + str(y1) + ' ' + str(z1) + '\n')
+    elif dataready:
+        (x2, y2, z2) = itg3205.getDegPerSecAxes()
+        gyrowriter.write(str(x2) + ' ' + str(y2) + ' ' + str(z2) + '\n')
+    else:
+        pass
