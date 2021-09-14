d7066e47cf8a335e87cfe425a807825a8e507735
helixing <helixing99@gmail.com>
2021-09-14 09:06:16 +0800

    1

--- GY-85_Raspberry-Pi/accTest.py
+++ GY-85_Raspberry-Pi/accTest.py
@@ -3,8 +3,18 @@ sys.path.insert(1,'/home/pi/audioproject/GY-85_Raspberry-Pi/i2clibraries')
 from i2c_adxl345 import *
 from time import *
 adxl345 = i2c_adxl345(1)
-a = 0
-time_start = time()
+def test_samplerate():
+    time_start = time()
+    a = 0
+    while (a<1000):
+        if adxl345.getInterruptStatus():
+            a = a + 1
+            (x, y, z) = adxl345.getAxes()
+    return (time() - time_start) / a
+
+for rate in [0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F]:
+    adxl345.set_data_rate(rate)
+    print(test_samplerate())
 # with open("acc.txt", "w") as writer:
 #     while True:
 #         if adxl345.getInterruptStatus():
@@ -13,9 +23,5 @@ time_start = time()
 #             print((time() - time_start)/a)
 #             #data = [str(x), str(y), str(z), str(time())]
 #             # writer.write(" ".join(data) + '\n')
-while(a<1000):
-    a = a + 1
-    print((time() - time_start) / a)
-    # if adxl345.getInterruptStatus():
-    #     (x, y, z) = adxl345.getAxes()
+
 
--- GY-85_Raspberry-Pi/i2clibraries/i2c_adxl345.py
+++ GY-85_Raspberry-Pi/i2clibraries/i2c_adxl345.py
@@ -101,8 +101,8 @@ class i2c_adxl345:
 		self.setInactivityTime()
 		self.setFreeFallThreshold()
 		self.setFreeFallTime()
+		#self.setdatarate()
 
-		self.setdatarate()
 
 		
 	def __str__(self):
@@ -112,8 +112,8 @@ class i2c_adxl345:
 		ret_str += "Y:    "+str(y)+"\n"
 		ret_str += "Z:    "+str(z)+"\n"
 		return ret_str
-	def setdatarate(self):
-		self.setOption(self.BandwidthRate, 0x0E & 0x0F)
+	def setdatarate(self, rate):
+		self.setOption(self.BandwidthRate, rate & 0x0F)
 	def getdatarate(self):
 		return self.getOptions(self.BandwidthRate) & 0x0F
 	def wakeUp(self):
