b8d37ba4961376344f20a2a09331cff161c60b1e
helixing <helixing99@gmail.com>
2021-09-14 16:00:26 +0800

    1

--- GY-85_Raspberry-Pi/datarecord.py
+++ GY-85_Raspberry-Pi/datarecord.py
@@ -6,8 +6,9 @@ from i2c_hmc5883l import *
 import time
 a, b, c = 0, 0, 0
 adxl345 = i2c_adxl345(1)
-hmc5883l = i2c_hmc5883l(1)
+adxl345.setdatarate(0x0F)
 itg3205 = i2c_itg3205(1)
+hmc5883l = i2c_hmc5883l(1)
 hmc5883l.setContinuousMode()
 hmc5883l.setDeclination(3, 5)
 accwriter = open('acc.txt', 'w')
