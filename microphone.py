c51123ac21c89afa1c36769913572ca3bf1efdb2
helixing <helixing99@gmail.com>
2021-09-16 17:41:34 +0800

    1

--- microphone.py
+++ microphone.py
@@ -54,7 +54,7 @@ class TapTester(object):
                                  channels = CHANNELS,
                                  rate = RATE,
                                  input = True,
-                                 input_device_index = 0,
+                                 input_device_index = 2,
                                  frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
 
         return stream1
