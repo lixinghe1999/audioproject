f7244f0f61b0fc09cd60cd0270516b3ebc35797c
helixing <helixing99@gmail.com>
2021-09-16 19:55:50 +0800

    1

--- microphone.py
+++ microphone.py
@@ -1,10 +1,11 @@
 
 import pyaudio
 import threading
+import numpy as np
 
 INITIAL_TAP_THRESHOLD = 200
 INITIAL_TAP_THRESHOLD1 = 200
-FORMAT = pyaudio.paInt8
+FORMAT = pyaudio.paInt16
 SHORT_NORMALIZE = (1.0/32768.0)
 CHANNELS = 1
 RATE = 44100
@@ -12,8 +13,9 @@ INPUT_BLOCK_TIME = 0.0001
 INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
 def voice_record(stream, frames):
     block = stream.read(frames, exception_on_overflow=False)
-    print(type(block), len(block))
-    return block
+    decode = np.fromstring(block, 'int16')
+    print(type(decode), len(decode))
+    return 0
 
 def open_mic_stream(index, rate = 44100):
     stream = pyaudio.PyAudio().open(format = FORMAT,
