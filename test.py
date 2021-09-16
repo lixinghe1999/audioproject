8f7ed5dcb90b91931407b8fb4a9ca10bd3c4c958
helixing <helixing99@gmail.com>
2021-09-16 17:41:09 +0800

    1

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pyaudio
 def getaudiodevices():
     p = pyaudio.PyAudio()
     for i in range(p.get_device_count()):
+        print(i)
         print(p.get_device_info_by_index(i).get('name'))
 
 getaudiodevices()
