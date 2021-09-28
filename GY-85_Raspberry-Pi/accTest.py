6c81b8e8da11b868f020324a5dd2e515b25c57bd
helixing <helixing99@gmail.com>
2021-09-28 16:20:27 +0800

    1

--- GY-85_Raspberry-Pi/accTest.py
+++ GY-85_Raspberry-Pi/accTest.py
@@ -3,8 +3,10 @@ import time
 
 sys.path.insert(1,'/home/pi/audioproject/GY-85_Raspberry-Pi/i2clibraries')
 from i2c_adxl345 import *
-adxl345_1 = i2c_adxl345(1)
+#adxl345_1 = i2c_adxl345(1)
+
 adxl345_2 = i2c_adxl345(6)
+adxl345_2.setdatarate(0x0F)
 i = 0
 time_start = time.time()
 while (i<1000):
