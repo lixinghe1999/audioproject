ce3d95d77c1ec6b6806fc168d8050ea9b35f1a99
helixing <helixing99@gmail.com>
2021-10-06 13:56:06 +0800

    1

--- GY-85_Raspberry-Pi/bmi160.py
+++ GY-85_Raspberry-Pi/bmi160.py
@@ -6,10 +6,10 @@ sensor = Driver(0x68) # change address if needed
 print('Initialization done')
 sensor.set_accel_rate(13)
 time_start = time.time()
-for i in range(1000):
+for i in range(10000):
     if sensor.getIntDataReadyStatus():
         data = sensor.getAcceleration()
-print(1000/(time.time() - time_start))
+print(10000/(time.time() - time_start))
 #while True:
   #data = sensor.getAcceleration()
   # fetch all gyro and acclerometer values
