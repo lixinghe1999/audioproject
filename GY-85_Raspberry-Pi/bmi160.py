9975b6cf73621c7b84e0cb820abcbb1bedd18246
helixing <helixing99@gmail.com>
2021-10-06 14:09:13 +0800

    1

--- GY-85_Raspberry-Pi/bmi160.py
+++ GY-85_Raspberry-Pi/bmi160.py
@@ -8,13 +8,13 @@ i = 0
 while(i<1000):
     #if sensor.getIntDataReadyStatus():
     data = sensor.getAcceleration()
-    print({
-      'ax': data[0],
-      'ay': data[1],
-      'az': data[2]
-    })
+    # print({
+    #   'ax': data[0],
+    #   'ay': data[1],
+    #   'az': data[2]
+    # })
     i = i + 1
-    time.sleep(0.1)
+    #time.sleep(0.1)
 print(1000/(time.time() - time_start))
 #while True:
   #data = sensor.getAcceleration()
