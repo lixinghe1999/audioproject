8095e020035c10361fcff190d9c2558f5fed145e
helixing <helixing99@gmail.com>
2021-10-26 17:36:44 +0800

    1

--- Raspberry-Pi/datarecord.py
+++ Raspberry-Pi/datarecord.py
@@ -1,5 +1,5 @@
 
-from bmi160 import bmi160_accsave
+from bmi160 import bmi160_accsave, bmi160_gyrosave
 from gy85 import gy85_gyrosave, gy85_compasssave, gy85_accsave
 from mic import open_mic_stream, voice_record
 
@@ -25,14 +25,17 @@ if __name__ == "__main__":
     micframe = args.time * 44100
     if args.acctype == 0:
         thread1 = Process(target=bmi160_accsave, args=('bmiacc', bmiaccframe, port))
+        thread4 = Process(target=bmi160_gyrosave, args=('bmigryo', bmiaccframe, port))
         thread2 = Process(target=voice_record, args=('mic1', open_mic_stream(1, micframe), micframe))
         thread3 = Process(target=voice_record, args=('mic2', open_mic_stream(2, micframe), micframe))
         thread1.start()
         thread2.start()
         thread3.start()
+        thread4.start()
         thread1.join()
         thread2.join()
         thread3.join()
+        thread4.join()
     elif args.acctype == 1:
         thread1 = Process(target=gy85_accsave, args=('gyacc', gyaccframe, port))
         thread2 = Process(target=voice_record, args=('mic1', open_mic_stream(1, micframe), micframe))
