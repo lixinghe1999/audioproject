dbcffb94fac8aaefbb024eb1a9058db111d70cf8
helixing <helixing99@gmail.com>
2021-11-02 11:11:45 +0800

    1

--- Raspberry-Pi/datarecord.py
+++ Raspberry-Pi/datarecord.py
@@ -14,29 +14,28 @@ if __name__ == "__main__":
     parser.add_argument('--time', action = "store",type=int, default = 5, required=False, help='time of data recording')
     parser.add_argument('--compass', action="store", type=int, default = 0, required=False, help='use compass')
     parser.add_argument('--acctype', action="store", type=int, default=0, required=False, help='gy or bmi')
-    parser.add_argument('--port', action="store", type=int, default=1, required=False, help='port/ i2c bus')
     args = parser.parse_args()
 
     gyaccframe = args.time * 3000
     bmiaccframe = args.time * 1600
-    port = args.port
+    port = [0, 1]
     gyroframe = args.time * 1500
     compassframe = args.time * 15
     micframe = args.time * 44100
     if args.acctype == 0:
-        thread1 = Process(target=bmi160_accsave, args=('bmiacc', bmiaccframe, port))
-        thread5 = Process(target=bmi160_accsave, args=('bmiacc', bmiaccframe, 0))
-        #thread4 = Process(target=bmi160_gyrosave, args=('bmigryo', bmiaccframe, port))
-        # thread2 = Process(target=voice_record, args=('mic1', open_mic_stream(1), micframe))
-        # thread3 = Process(target=voice_record, args=('mic2', open_mic_stream(2), micframe))
+        thread1 = Process(target=bmi160_accsave, args=('bmiacc1', bmiaccframe, port[1]))
+        thread5 = Process(target=bmi160_accsave, args=('bmiacc2', bmiaccframe, port[0]))
+        # thread4 = Process(target=bmi160_gyrosave, args=('bmigryo', bmiaccframe, port))
+        thread2 = Process(target=voice_record, args=('mic1', open_mic_stream(1), micframe))
+        thread3 = Process(target=voice_record, args=('mic2', open_mic_stream(2), micframe))
         thread1.start()
-        # thread2.start()
-        # thread3.start()
+        thread2.start()
+        thread3.start()
         #thread4.start()
         thread5.start()
         thread1.join()
-        # thread2.join()
-        # thread3.join()
+        thread2.join()
+        thread3.join()
         #thread4.join()
         thread5.join()
     elif args.acctype == 1:
