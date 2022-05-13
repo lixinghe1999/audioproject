76baa6deaa17e8a0eaa23f52dc326a0be8e3e1a4
lixinghe1999 <helixing99@gmail.com>
2022-05-13 16:49:04 +0800

    final revision

--- DL/data.py
+++ DL/data.py
@@ -282,13 +282,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.mode == 0:
         audio_files = []
-        for path in [r"../dataset/dev-clean", r"../dataset/music", r"../dataset/background"]:
+        #for path in [r"../dataset/dev-clean", r"../dataset/music", r"../dataset/background"]:
+        for path in [r"../data/train-clean-100"]:
             g = os.walk(path)
             for path, dir_list, file_list in g:
                 for file_name in file_list:
                     if file_name[-3:] not in ['txt', 'mp3']:
                         audio_files.append([os.path.join(path, file_name), torchaudio.info(os.path.join(path, file_name)).num_frames])
-        json.dump(audio_files, open('json/all_noise.json', 'w'), indent=4)
+        json.dump(audio_files, open('json/speech.json', 'w'), indent=4)
     elif args.mode == 1:
         person = ["liang", "he", "hou", "shi", "shuai", "wu", "yan", "jiang", "1", "2", "3", "4", "5", "6", "7", "8", "airpod", "galaxy", 'freebud']
         for folder, name in zip(['noise_train', 'train', 'mobile', 'clean', 'noise'], ['json/noise_train', 'json/clean_train', 'json/mobile', 'json/clean', 'json/noise']):
