381170a786add62815511f6d83f3a2619f194980
lixinghe1999 <helixing99@gmail.com>
2022-05-13 16:27:03 +0800

    final revision

--- DL/data.py
+++ DL/data.py
@@ -312,9 +312,9 @@ if __name__ == "__main__":
                         print(os.path.join(path, imu1[i]))
                         imu_files.append([os.path.join(path, imu1[i]), len(open(os.path.join(path, imu1[i]), encoding='windows-1254').readlines())])
                         wav_files.append([os.path.join(path, wav[i]), torchaudio.info(os.path.join(path, wav[i])).num_frames])
-                        gt_files.append([os.path.join(path, gt[i]), torchaudio.info(os.path.join(path, gt[i]), encoding='windows-1254').num_frames])
+                        gt_files.append([os.path.join(path, gt[i]), torchaudio.info(os.path.join(path, gt[i])).num_frames])
 
-                        imu_files.append([os.path.join(path, imu2[i]), len(open(os.path.join(path, imu2[i])).readlines())])
+                        imu_files.append([os.path.join(path, imu2[i]), len(open(os.path.join(path, imu2[i]), encoding='windows-1254').readlines())])
                         wav_files.append([os.path.join(path, wav[i]), torchaudio.info(os.path.join(path, wav[i])).num_frames])
                         gt_files.append([os.path.join(path, gt[i]), torchaudio.info(os.path.join(path, gt[i])).num_frames])
                 json.dump(imu_files, open(name + '_imuexp7.json', 'w'), indent=4)
