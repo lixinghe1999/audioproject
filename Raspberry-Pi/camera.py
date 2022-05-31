371d66b8b028e54461191f3bb80531a20e0cb882
lixinghe1999 <helixing99@gmail.com>
2022-05-31 17:12:43 +0800

    final revision

--- Raspberry-Pi/camera.py
+++ Raspberry-Pi/camera.py
@@ -6,10 +6,9 @@ def webcam_save(name, num):
 	t_start = time.time()
 	fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 	out = cv2.VideoWriter(name + '_' + str(t_start) + '.mp4', fourcc, 20.0, (640, 480))
-	count = 0
 	for i in range(num):
 		ret, frame = cap.read()
-		print(count)
+		print(i)
 		out.write(frame)
 	cv2.destroyAllWindows()
 	cap.release()
