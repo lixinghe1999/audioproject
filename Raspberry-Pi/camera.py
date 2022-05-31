9b697f8fccd047d0328f12ba800ec99cb349d65d
lixinghe1999 <helixing99@gmail.com>
2022-05-31 17:12:16 +0800

    final revision

--- Raspberry-Pi/camera.py
+++ Raspberry-Pi/camera.py
@@ -2,19 +2,15 @@ import cv2
 import time
 cap = cv2.VideoCapture(0)
 
-def webcam_save(name, duration):
+def webcam_save(name, num):
 	t_start = time.time()
 	fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 	out = cv2.VideoWriter(name + '_' + str(t_start) + '.mp4', fourcc, 20.0, (640, 480))
 	count = 0
-	while(cap.isOpened()):
+	for i in range(num):
 		ret, frame = cap.read()
 		print(count)
-		count += 1
-		if ret==True:
-			out.write(frame)
-		if (time.time() - t_start) > duration:
-			break
+		out.write(frame)
 	cv2.destroyAllWindows()
 	cap.release()
 	out.release()
--- Raspberry-Pi/datarecord.py
+++ Raspberry-Pi/datarecord.py
@@ -11,20 +11,21 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--time', action = "store",type=int, default = 5, required=False, help='time of data recording')
+    parser.add_argument('--time', action = "store",type=int, default=5, required=False, help='time of data recording')
     parser.add_argument('--mode', action = "store", type=int, default=0, required=False, help='whether have microphone')
     parser.add_argument('--device', action="store", type=int, default=0, required=False, help='device number of microphone')
     args = parser.parse_args()
 
     bmiaccframe = args.time * 1600
     micframe = args.time * 16000
+    camframe = args.time * 20
     device = args.device
     if args.mode == 0:
         stream = open_mic_stream(device)
         thread1 = Process(target=bmi160_accsave, args=('bmiacc1', bmiaccframe, 0))
         thread2 = Process(target=bmi160_accsave, args=('bmiacc2', bmiaccframe, 1))
         thread = Process(target=voice_record, args=('mic', stream, micframe))
-        thread_camera = Process(target=webcam_save, args=('cam', args.time))
+        thread_camera = Process(target=webcam_save, args=('cam', camframe))
 
         thread1.start()
         thread2.start()
