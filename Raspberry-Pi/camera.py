806982d43133c977a74b7d7e3efed396f3fe70de
lixinghe1999 <helixing99@gmail.com>
2022-05-31 17:03:50 +0800

    final revision

--- Raspberry-Pi/camera.py
+++ Raspberry-Pi/camera.py
@@ -11,7 +11,8 @@ def webcam_save(name, duration):
 	# 	time.sleep(0.04)
 	# 	cv2.imwrite(name + '_' + str(time.time()) + '.jpg', image)
 	t_start = time.time()
-	out = cv2.VideoWriter(name + '_' + str(t_start) + '.avi', -1, 20.0, (640, 480))
+	fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+	out = cv2.VideoWriter(name + '_' + str(t_start) + '.mp4', fourcc, 20.0, (640, 480))
 	while(cap.isOpened()):
 		ret, frame = cap.read()
 		if ret==True:
