301d056ede15e3b6c3f29c1dbc87d2e1741b346a
lixinghe1999 <helixing99@gmail.com>
2022-07-13 15:40:19 +0800

    delete SepFormer

--- DL/train.py
+++ DL/train.py
@@ -74,13 +74,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.mode == 0:
-        BATCH_SIZE = 1
+        BATCH_SIZE = 32
         lr = 0.001
         EPOCH = 30
         dataset = NoisyCleanSet(['json/train.json', 'json/dev.json'], simulation=True, ratio=1)
 
         #model = nn.DataParallel(A2net(), device_ids=[0]).to(device)
-        model = Model(num_freqs=264).to(device)
+        model = nn.DataParallel(Model(num_freqs=264), device_ids=[0, 1]).to(device)
         ckpt_best, loss_curve = train(dataset, EPOCH, lr, BATCH_SIZE, model, save_all=True)
 
         plt.plot(loss_curve)
