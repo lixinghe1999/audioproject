f03eac5323e5614f909fee9faaf540da0ad4222e
lixinghe1999 <helixing99@gmail.com>
2022-07-21 11:23:10 +0800

    baseline data

--- DL/train.py
+++ DL/train.py
@@ -141,13 +141,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.mode == 0:
-        BATCH_SIZE = 128
+        BATCH_SIZE = 32
         lr = 0.001
         EPOCH = 30
         dataset = NoisyCleanSet(['json/train.json', 'json/all_noise.json'], simulation=True, ratio=1)
 
-        model = nn.DataParallel(A2net(), device_ids=[0]).to(device)
-        #model = nn.DataParallel(Model(num_freqs=264).to(device), device_ids=[0, 1])
+        #model = nn.DataParallel(A2net(), device_ids=[0]).to(device)
+        model = nn.DataParallel(Model(num_freqs=264).to(device), device_ids=[0, 1])
         ckpt_best, loss_curve = train(dataset, EPOCH, lr, BATCH_SIZE, model, save_all=True)
 
         plt.plot(loss_curve)
