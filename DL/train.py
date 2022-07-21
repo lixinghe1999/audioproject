cfd7bbd359c024857d9cc83656b6267fb75d2b28
lixinghe1999 <helixing99@gmail.com>
2022-07-21 21:04:09 +0800

    train fullsubnet

--- DL/train.py
+++ DL/train.py
@@ -88,6 +88,8 @@ def sample(model, x, noise, y, audio_only=False):
 def train(dataset, EPOCH, lr, BATCH_SIZE, model, save_all=False):
     length = len(dataset)
     test_size = min(int(0.1 * length), 2000)
+    test_size = int(0.4 * length)
+
     train_size = length - test_size
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
     train_loader = Data.DataLoader(dataset=train_dataset, num_workers=8, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
@@ -135,9 +137,7 @@ def train(dataset, EPOCH, lr, BATCH_SIZE, model, save_all=False):
 def inference(dataset, BATCH_SIZE, model):
     length = len(dataset)
     test_size = min(int(0.1 * length), 2000)
-    print(test_size)
-    test_size = int(0.4 * length)
-    print(test_size)
+
     train_size = length - test_size
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
     test_loader = Data.DataLoader(dataset=test_dataset, num_workers=4, batch_size=BATCH_SIZE, shuffle=False)
