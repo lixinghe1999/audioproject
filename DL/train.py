00d84b24f04a5d66e1463282310ebabe89a3ee74
lixinghe1999 <helixing99@gmail.com>
2022-07-27 10:08:25 +0800

    new balance fullsubnet

--- DL/train.py
+++ DL/train.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
         lr = 0.001
         EPOCH = 10
 
-        ckpt_dir = 'pretrain/vibvoice_new'
+        ckpt_dir = 'pretrain/fullsubnet'
         ckpt_name = ckpt_dir + '/' + sorted(os.listdir(ckpt_dir))[0]
         ckpt = torch.load(ckpt_name)
 
@@ -190,19 +190,19 @@ if __name__ == "__main__":
         dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'], person=people, simulation=True)
 
         model.load_state_dict(ckpt)
-        ckpt, loss_curve, metric_best = train(dataset, EPOCH, lr, BATCH_SIZE, model, audio_only=False, complex=False)
+        ckpt, loss_curve, metric_best = train(dataset, EPOCH, lr, BATCH_SIZE, model, audio_only=True, complex=True)
 
         # Optional Micro-benchmark
         model.load_state_dict(ckpt)
         people = ["1", "2", "3", "4", "5", "6", "7", "8", "yan", "wu", "liang", "shuai", "shi", "he", "hou"]
         for noise in ['dev.json', 'background.json', 'music.json']:
             dataset = NoisyCleanSet(['json/train_gt.json', 'json/' + noise, 'json/train_imu.json'], person=people, simulation=True)
-            avg_metric = inference(dataset, BATCH_SIZE, model, audio_only=False, complex=False)
+            avg_metric = inference(dataset, BATCH_SIZE, model, audio_only=True, complex=True)
             print(noise, avg_metric)
 
         for level in [1, 6, 11]:
             dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'], person=people, simulation=True, snr=[level-1, level+1])
-            avg_metric = inference(dataset, BATCH_SIZE, model, audio_only=False, complex=False)
+            avg_metric = inference(dataset, BATCH_SIZE, model, audio_only=True, complex=True)
             print(level, avg_metric)
 
     elif args.mode == 2:
