bfd05a1cb497a30b1d77f33b097e016ddd652081
lixinghe1999 <helixing99@gmail.com>
2022-08-02 10:17:52 +0800

    two channel imu

--- DL/json_generate.py
+++ DL/json_generate.py
@@ -12,56 +12,64 @@ def load(path, files, audio=False):
             log.append([os.path.join(path, f), len(open(os.path.join(path, f), 'rb').readlines())])
     return log
 
-def update(dict, name, file_list, N, p):
+def update(dict, name, file_list, N, p, kinds=4):
     file_list = sorted(file_list)
-    if N % 4==0:
-        N = int(N/4)
-        # imu1 = file_list[: N]
-        # imu2 = file_list[N: 2 * N]
-        imu = file_list[: 2 * N]
-        gt = file_list[2 * N: 3 * N]
-        wav = file_list[3 * N:]
-        imu_files = load(path, imu, audio=False)
-        wav_files = load(path, wav, audio=True)
-        gt_files = load(path, gt, audio=True)
-        if name in dict:
-            if p in dict[name][0]:
-                dict[name][0][p] += imu_files
-                dict[name][1][p] += wav_files
-                dict[name][2][p] += gt_files
-            else:
-                dict[name][0][p] = imu_files
-                dict[name][1][p] = wav_files
-                dict[name][2][p] = gt_files
+    N = int(N / kinds)
+    imu = file_list[: 2 * N]
+    gt = file_list[2 * N: 3 * N]
+    wav = file_list[3 * N:]
+    imu_files = load(path, imu, audio=False)
+    gt_files = load(path, gt, audio=True)
+    wav_files = load(path, wav, audio=True)
+    if name in dict:
+        if p in dict[name][0]:
+            dict[name][0][p] += imu_files
+            dict[name][1][p] += gt_files
+            dict[name][2][p] += wav_files
         else:
-            dict[name] = [{p: imu_files}, {p: wav_files}, {p: gt_files}]
+            dict[name][0][p] = imu_files
+            dict[name][1][p] = gt_files
+            dict[name][2][p] = wav_files
     else:
-        N = int(N / 3)
-        # imu1 = file_list[: N]
-        # imu2 = file_list[N: 2 * N]
-        imu = file_list[: 2 * N]
-        gt = file_list[2 * N: 3 * N]
-        imu_files = load(path, imu, audio=False)
-        gt_files = load(path, gt, audio=True)
-        if name in dict:
-            if p in dict[name][0]:
-                dict[name][0][p] += imu_files
-                dict[name][1][p] += gt_files
-            else:
-                dict[name][0][p] = imu_files
-                dict[name][1][p] = gt_files
-        else:
-            dict[name] = [{p: imu_files}, {p: gt_files}]
-    #
-    # for i in range(N):
-    #     imu_files.append([os.path.join(path, imu1[i]), len(open(os.path.join(path, imu1[i]), 'rb').readlines())])
-    #     wav_files.append([os.path.join(path, wav[i]), torchaudio.info(os.path.join(path, wav[i])).num_frames])
-    #     gt_files.append([os.path.join(path, gt[i]), torchaudio.info(os.path.join(path, gt[i])).num_frames])
-    #
-    #     imu_files.append([os.path.join(path, imu2[i]), len(open(os.path.join(path, imu2[i]), 'rb').readlines())])
-    #     wav_files.append([os.path.join(path, wav[i]), torchaudio.info(os.path.join(path, wav[i])).num_frames])
-    #     gt_files.append([os.path.join(path, gt[i]), torchaudio.info(os.path.join(path, gt[i])).num_frames])
-
+        dict[name] = [{p: imu_files}, {p: gt_files}, {p: wav_files}]
+    # if N % 4 == 0:
+    #     N = int(N/4)
+    #     # imu1 = file_list[: N]
+    #     # imu2 = file_list[N: 2 * N]
+    #     imu = file_list[: 2 * N]
+    #     gt = file_list[2 * N: 3 * N]
+    #     wav = file_list[3 * N:]
+    #     imu_files = load(path, imu, audio=False)
+    #     wav_files = load(path, wav, audio=True)
+    #     gt_files = load(path, gt, audio=True)
+    #     if name in dict:
+    #         if p in dict[name][0]:
+    #             dict[name][0][p] += imu_files
+    #             dict[name][1][p] += wav_files
+    #             dict[name][2][p] += gt_files
+    #         else:
+    #             dict[name][0][p] = imu_files
+    #             dict[name][1][p] = wav_files
+    #             dict[name][2][p] = gt_files
+    #     else:
+    #         dict[name] = [{p: imu_files}, {p: wav_files}, {p: gt_files}]
+    # else:
+    #     N = int(N / 3)
+    #     # imu1 = file_list[: N]
+    #     # imu2 = file_list[N: 2 * N]
+    #     imu = file_list[: 2 * N]
+    #     gt = file_list[2 * N: 3 * N]
+    #     imu_files = load(path, imu, audio=False)
+    #     gt_files = load(path, gt, audio=True)
+    #     if name in dict:
+    #         if p in dict[name][0]:
+    #             dict[name][0][p] += imu_files
+    #             dict[name][1][p] += gt_files
+    #         else:
+    #             dict[name][0][p] = imu_files
+    #             dict[name][1][p] = gt_files
+    #     else:
+    #         dict[name] = [{p: imu_files}, {p: gt_files}]
     return dict
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -69,16 +77,16 @@ if __name__ == "__main__":
                         help='mode of processing, 0-pre train, 1-main benchmark, 2-mirco benchmark')
     args = parser.parse_args()
     if args.mode == 0:
-        # directory = "../dataset/"
-        # datasets = ['dev', 'background', 'music', 'train']
-        # for dataset in datasets:
-        #     audio_files = []
-        #     g = os.walk(directory + dataset)
-        #     for path, dir_list, file_list in g:
-        #         for file_name in file_list:
-        #             if file_name[-3:] in ['wav', 'lac']:
-        #                 audio_files.append([os.path.join(path, file_name), torchaudio.info(os.path.join(path, file_name)).num_frames])
-        #     json.dump(audio_files, open('json/' + dataset + '.json', 'w'), indent=4)
+        directory = "../dataset/"
+        datasets = ['dev', 'background', 'music', 'train']
+        for dataset in datasets:
+            audio_files = []
+            g = os.walk(directory + dataset)
+            for path, dir_list, file_list in g:
+                for file_name in file_list:
+                    if file_name[-3:] in ['wav', 'lac']:
+                        audio_files.append([os.path.join(path, file_name), torchaudio.info(os.path.join(path, file_name)).num_frames])
+            json.dump(audio_files, open('json/' + dataset + '.json', 'w'), indent=4)
 
         data = {}
         for dataset in ['dev', 'background', 'music']:
@@ -114,10 +122,10 @@ if __name__ == "__main__":
                     name = path.split('/')[-1]
                     dict = update(dict, name, file_list, N, p)
         for name in dict:
-            if len(dict[name]) == 2:
+            # if len(dict[name]) == 2:
+            #     json.dump(dict[name][0], open('json/' + name + '_imu.json', 'w'), indent=4)
+            #     json.dump(dict[name][1], open('json/' + name + '_gt.json', 'w'), indent=4)
+            # else:
                 json.dump(dict[name][0], open('json/' + name + '_imu.json', 'w'), indent=4)
                 json.dump(dict[name][1], open('json/' + name + '_gt.json', 'w'), indent=4)
-            else:
-                json.dump(dict[name][0], open('json/' + name + '_imu.json', 'w'), indent=4)
-                json.dump(dict[name][1], open('json/' + name + '_wav.json', 'w'), indent=4)
-                json.dump(dict[name][2], open('json/' + name + '_gt.json', 'w'), indent=4)
+                json.dump(dict[name][2], open('json/' + name + '_wav.json', 'w'), indent=4)
