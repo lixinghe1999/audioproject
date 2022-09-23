cde38a4fd2fcc7eae960b470235e7f40bcadb2d9
lixinghe1999 <helixing99@gmail.com>
2022-09-23 13:51:59 +0800

    drop_last

--- authentication/experiment.py
+++ authentication/experiment.py
@@ -128,88 +128,81 @@ class Experiment():
         plt.savefig(str(best_acc) + '_acc.png')
 
     def contrastive_test(self):
-        overall_avg_EER = 0
-        for e in range(50):
-            batch_avg_EER = 0
-            for batch_id, embeddings in enumerate(self.test_loader):
+        batch_avg_EER = 0
+        for batch_id, embeddings in enumerate(self.test_loader):
+            embeddings = embeddings.to(device=self.device, dtype=torch.float)
+            assert self.params['num_utterances'] % 2 == 0
+
+            enrollment_batch, verification_batch = torch.split(embeddings, int(embeddings.size(1) / 2), dim=1)
+            enrollment_batch = torch.reshape(enrollment_batch, (
+                self.params['test_batch_size'] * self.params['num_utterances'] // 2,
+                enrollment_batch.size(2), enrollment_batch.size(3), enrollment_batch.size(4)))
+            verification_batch = torch.reshape(verification_batch, (
+                self.params['test_batch_size'] * self.params['num_utterances'] // 2,
+                verification_batch.size(2), verification_batch.size(3), verification_batch.size(4)))
+
+            perm = random.sample(range(0, verification_batch.size(0)), verification_batch.size(0))
+            unperm = list(perm)
+            for i, j in enumerate(perm):
+                unperm[j] = i
+
+            verification_batch = verification_batch[perm]
+            enrollment_embeddings = self.model(enrollment_batch)
+            verification_embeddings = self.model(verification_batch)
+
+            verification_embeddings = verification_embeddings[unperm]
+
+            enrollment_embeddings = torch.reshape(enrollment_embeddings,
+                (self.params['test_batch_size'], self.params['num_utterances'] // 2, enrollment_embeddings.size(1)))
+            verification_embeddings = torch.reshape(verification_embeddings, (
+                self.params['test_batch_size'], self.params['num_utterances'] // 2, verification_embeddings.size(1)))
+
+            enrollment_centroids = get_centroids(enrollment_embeddings)
+            sim_matrix = get_cossim(verification_embeddings, enrollment_centroids)
+
+            # calculating EER
+            diff = 1;
+            EER = 0;
+
+            for thres in [0.01 * i + 0.5 for i in range(50)]:
+                sim_matrix_thresh = sim_matrix > thres
+
+                FAR = (sum([sim_matrix_thresh[i].float().sum() - sim_matrix_thresh[i, :, i].float().sum() for i
+                            in range(self.params['test_batch_size'])])
+                       / (self.params['test_batch_size'] - 1.0) / (float(self.params['num_utterances'] / 2)) / self.params['batch_size'])
+
+                FRR = (sum([self.params['num_utterances'] / 2 - sim_matrix_thresh[i, :, i].float().sum() for i in
+                            range(self.params['test_batch_size'])])
+                       / (float(self.params['num_utterances']/ 2)) / self.params['test_batch_size'])
+
+                # Save threshold when FAR = FRR (=EER)
+                if diff > abs(FAR - FRR):
+                    diff = abs(FAR - FRR)
+                    EER = (FAR + FRR) / 2
+            batch_avg_EER += EER
+        batch_avg_EER = batch_avg_EER / (batch_id + 1)
+        print("\n average EER: %0.2f" % (batch_avg_EER))
+    def constrastive_train(self):
+        for i in range(self.params['epoch']):
+            for embeddings in self.train_loader:
                 embeddings = embeddings.to(device=self.device, dtype=torch.float)
-                assert self.params['num_utterances'] % 2 == 0
-
-                enrollment_batch, verification_batch = torch.split(embeddings, int(embeddings.size(1) / 2), dim=1)
-                enrollment_batch = torch.reshape(enrollment_batch, (
-                    self.params['test_batch_size'] * self.params['num_utterances'] // 2,
-                    enrollment_batch.size(2), enrollment_batch.size(3), enrollment_batch.size(4)))
-                verification_batch = torch.reshape(verification_batch, (
-                    self.params['test_batch_size'] * self.params['num_utterances'] // 2,
-                    verification_batch.size(2), verification_batch.size(3), verification_batch.size(4)))
-
-                perm = random.sample(range(0, verification_batch.size(0)), verification_batch.size(0))
+                embeddings = torch.reshape(embeddings, (self.params['train_batch_size'] * self.params['num_utterances'], 2, 33, 151))
+                perm = random.sample(range(0, self.params['train_batch_size'] * self.params['num_utterances']),
+                                     self.params['train_batch_size'] * self.params['num_utterances'])
                 unperm = list(perm)
                 for i, j in enumerate(perm):
                     unperm[j] = i
+                embeddings = embeddings[perm]
+                # gradient accumulates
+                self.optimizer.zero_grad()
+                embeddings = self.model(embeddings)
+                embeddings = embeddings[unperm]
+                embeddings = torch.reshape(embeddings, (self.params['train_batch_size'], self.params['num_utterances'], -1))
 
-                verification_batch = verification_batch[perm]
-                enrollment_embeddings = self.model(enrollment_batch)
-                verification_embeddings = self.model(verification_batch)
-
-                verification_embeddings = verification_embeddings[unperm]
-
-                enrollment_embeddings = torch.reshape(enrollment_embeddings,
-                    (self.params['test_batch_size'], self.params['num_utterances'] // 2, enrollment_embeddings.size(1)))
-                verification_embeddings = torch.reshape(verification_embeddings, (
-                    self.params['test_batch_size'], self.params['num_utterances'] // 2, verification_embeddings.size(1)))
-
-                enrollment_centroids = get_centroids(enrollment_embeddings)
-                sim_matrix = get_cossim(verification_embeddings, enrollment_centroids)
-
-                # calculating EER
-                diff = 1;
-                EER = 0;
-
-                for thres in [0.01 * i + 0.5 for i in range(50)]:
-                    sim_matrix_thresh = sim_matrix > thres
-
-                    FAR = (sum([sim_matrix_thresh[i].float().sum() - sim_matrix_thresh[i, :, i].float().sum() for i
-                                in range(self.params['test_batch_size'])])
-                           / (self.params['test_batch_size'] - 1.0) / (float(self.params['num_utterances'] / 2)) / self.params['batch_size'])
-
-                    FRR = (sum([self.params['num_utterances'] / 2 - sim_matrix_thresh[i, :, i].float().sum() for i in
-                                range(self.params['test_batch_size'])])
-                           / (float(self.params['num_utterances']/ 2)) / self.params['test_batch_size'])
-
-                    # Save threshold when FAR = FRR (=EER)
-                    if diff > abs(FAR - FRR):
-                        diff = abs(FAR - FRR)
-                        EER = (FAR + FRR) / 2
-                batch_avg_EER += EER
-                #print("\nEER : %0.2f (thres:%0.2f, FAR:%0.2f, FRR:%0.2f)" % (EER, EER_thresh, EER_FAR, EER_FRR))
-            batch_avg_EER = batch_avg_EER / (batch_id + 1)
-            overall_avg_EER += batch_avg_EER
-        overall_avg_EER = overall_avg_EER / (e + 1)
-        print("\n average EER: %0.2f" % (overall_avg_EER))
-    def constrastive_train(self):
-        for i in range(self.params['epoch']):
-            for e in tqdm(range(100)): # mini-epoch
-                for embeddings in self.train_loader:
-                    embeddings = embeddings.to(device=self.device, dtype=torch.float)
-                    print(embeddings.shape)
-                    embeddings = torch.reshape(embeddings, (self.params['train_batch_size'] * self.params['num_utterances'], 2, 33, 151))
-                    perm = random.sample(range(0, self.params['train_batch_size'] * self.params['num_utterances']),
-                                         self.params['train_batch_size'] * self.params['num_utterances'])
-                    unperm = list(perm)
-                    for i, j in enumerate(perm):
-                        unperm[j] = i
-                    embeddings = embeddings[perm]
-                    # gradient accumulates
-                    self.optimizer.zero_grad()
-                    embeddings = self.model(embeddings)
-                    embeddings = embeddings[unperm]
-                    embeddings = torch.reshape(embeddings, (self.params['train_batch_size'], self.params['num_utterances'], -1))
-
-                    # get loss, call backward, step optimizer
-                    loss = self.loss(embeddings)  # wants (Speaker, Utterances, embedding)
-                    loss.backward()
-                    self.optimizer.step()
+                # get loss, call backward, step optimizer
+                loss = self.loss(embeddings)  # wants (Speaker, Utterances, embedding)
+                loss.backward()
+                self.optimizer.step()
             with torch.no_grad():
                 self.contrastive_test()
             self.scheduler.step()
