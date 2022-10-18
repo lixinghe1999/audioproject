7f806718280a83bd04c0a6996f6d495a71b1658f
lixinghe1999 <helixing99@gmail.com>
2022-10-18 15:42:04 +0800

    re-train fullsubnet

--- enhancement/train.py
+++ enhancement/train.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     torch.cuda.set_device(1)
     if args.mode == 0:
         # This script is for model pre-training on LibriSpeech
-        BATCH_SIZE = 16
+        BATCH_SIZE = 8
         lr = 0.01
         EPOCH = 30
         dataset = NoisyCleanSet(['json/train.json', 'json/all_noise.json'], simulation=True, ratio=0.1)
