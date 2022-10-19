88de8f475b2d5369df28daf9e71649e870e92229
lixinghe1999 <helixing99@gmail.com>
2022-10-19 13:53:36 +0800

    new evaluation + scheduler

--- enhancement/discriminator.py
+++ enhancement/discriminator.py
@@ -12,14 +12,6 @@ def pesq_loss(clean, noisy, sr=16000):
         pesq_score = -1
     return pesq_score
 
-def batch_pesq(clean, noisy):
-    pesq_score = Parallel(n_jobs=-1)(delayed(pesq_loss)(c, n) for c, n in zip(clean, noisy))
-    pesq_score = np.array(pesq_score)
-    if -1 in pesq_score:
-        return None
-    pesq_score = (pesq_score - 1) / 3.5
-    return torch.FloatTensor(pesq_score).to('cuda')
-
 class LearnableSigmoid(nn.Module):
     def __init__(self, in_features, beta=1):
         super().__init__()
@@ -54,6 +46,14 @@ class Discriminator(nn.Module):
             nn.utils.spectral_norm(nn.Linear(ndf*4, 1)),
             LearnableSigmoid(1)
         )
+    def batch_pesq(self, clean, noisy):
+
+        pesq_score = Parallel(n_jobs=-1)(delayed(pesq_loss)(c, n) for c, n in zip(clean, noisy))
+        pesq_score = np.array(pesq_score)
+        if -1 in pesq_score:
+            return None
+        pesq_score = (pesq_score - 1) / 3.5
+        return torch.FloatTensor(pesq_score).to('cuda')
 
     def forward(self, x, y):
         xy = torch.cat([x, y], dim=1)
