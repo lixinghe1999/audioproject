5cae8a1855fce9f6fe467706209b325b7205f16d
lixinghe1999 <helixing99@gmail.com>
2022-10-28 12:50:35 +0800

    asr re-compute

--- enhancement/dataset.py
+++ enhancement/dataset.py
@@ -293,6 +293,7 @@ class NoisyCleanSet:
             # clean = clean[:, : 8 * (freq_bin_high), :-1]
             # imu = imu[:, :, :-1]
         if self.text:
+            print(file)
             setence = sentences[int(file.split('/')[4][-1])]
             print(file, setence)
             return setence, imu, noise, clean
