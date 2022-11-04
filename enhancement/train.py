09818df56adc8fc622276b0a7697d10544464bf9
lixinghe1999 <helixing99@gmail.com>
2022-11-04 09:35:54 +0800

    wer result fullsubnet

--- enhancement/train.py
+++ enhancement/train.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     # model = TSCNet().to(device)
     # model = SEANet().to(device)
 
-    #model = nn.DataParallel(model, device_ids=[0, 1])
+    model = nn.DataParallel(model, device_ids=[0, 1])
 
     # discriminator = Discriminator_spectrogram().to(device)
     discriminator = Discriminator_time().to(device)
