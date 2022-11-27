18f870767f825e5a81e908801798995a1b4b2a96
lixinghe1999 <helixing99@gmail.com>
2022-11-27 15:09:24 +0800

    fine-tune for SEANet

--- enhancement/train.py
+++ enhancement/train.py
@@ -241,15 +241,16 @@ if __name__ == "__main__":
         #     metric = inference(test_dataset, 4, model)
         #     avg_metric = np.mean(metric, axis=0)
         #     print(p, avg_metric)
-        # for env in ['airpod', 'freebud', 'galaxy', 'office', 'corridor', 'stair']:
-        #     test_dataset = NoisyCleanSet(['json/noise_gt.json', 'json/noise_wav.json', 'json/noise_imu.json'],
-        #                                  person=[env], time_domain=time_domain, simulation=False, text=True)
-        #     metric = inference(test_dataset, 4, model)
-        #     avg_metric = np.mean(metric, axis=0)
-        #     print(env, avg_metric)
+        #for env in ['airpod', 'freebud', 'galaxy', 'office', 'corridor', 'stair']:
+        for env in ['office', 'corridor', 'stair']:
+            test_dataset = NoisyCleanSet(['json/noise_gt.json', 'json/noise_wav.json', 'json/noise_imu.json'],
+                                         person=[env], time_domain=time_domain, simulation=False, text=True)
+            metric = inference(test_dataset, 4, model)
+            avg_metric = np.mean(metric, axis=0)
+            print(env, avg_metric)
 
-        test_dataset = NoisyCleanSet(['json/mobile_gt.json', 'json/mobile_wav.json', 'json/mobile_imu.json'],
-                                     person=['he'], time_domain=time_domain, simulation=False, text=True)
-        metric = inference(test_dataset, 4, model)
-        avg_metric = np.mean(metric, axis=0)
-        print('mobile result', avg_metric)
+        # test_dataset = NoisyCleanSet(['json/mobile_gt.json', 'json/mobile_wav.json', 'json/mobile_imu.json'],
+        #                              person=['he'], time_domain=time_domain, simulation=False, text=True)
+        # metric = inference(test_dataset, 4, model)
+        # avg_metric = np.mean(metric, axis=0)
+        # print('mobile result', avg_metric)
