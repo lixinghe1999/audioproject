e8e96f5bfb2621b233a4b645ecec2cb83363aa86
lixinghe1999 <helixing99@gmail.com>
2022-11-28 14:35:52 +0800

    test the WER before processing

--- enhancement/train.py
+++ enhancement/train.py
@@ -47,7 +47,7 @@ def train(dataset, EPOCH, lr, BATCH_SIZE, model, discriminator=None, save_all=Fa
     optimizer = torch.optim.Adam(params=model.parameters(), lr=lr, betas=(0.9, 0.999))
     if discriminator is not None:
         optimizer_disc = torch.optim.AdamW(params=discriminator.parameters(), lr=lr, betas=(0.9, 0.999))
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.5)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
     loss_best = 100
     loss_curve = []
     ckpt_best = model.state_dict()
@@ -222,26 +222,25 @@ if __name__ == "__main__":
         ckpt_start = torch.load(ckpt_name)
         people = ["hou", "1", "2", "3", "4", "5", "6", "7", "8", "yan", "wu", "liang", "shuai", "shi", "he"]
         ckpts = []
-        # for p in people:
-        #     model.load_state_dict(ckpt_start)
-        #     p_except = [i for i in people if i != p]
-        #
-        #     # train_dataset = NoisyCleanSet(['json/noise_train_gt.json', 'json/dev.json', 'json/noise_train_imu.json'],
-        #     #                               person=p_except, time_domain=time_domain, simulation=True, text=False)
-        #     # ckpt, _, _ = train(train_dataset, 5, 0.0001, 16, model)
-        #
-        #     train_dataset = NoisyCleanSet(['json/noise_train_gt.json', 'json/noise_train_wav.json', 'json/noise_train_imu.json'],
-        #                                  person=p_except, time_domain=time_domain, simulation=False, text=False)
-        #     ckpt, _, _ = train(train_dataset, 5, 0.0002, 16, model)
-        #
-        #     train_dataset = NoisyCleanSet(['json/train_gt.json', 'json/dev.json', 'json/train_imu.json'],
-        #                                   person=[p], time_domain=time_domain, simulation=True, text=False)
-        #     model.load_state_dict(ckpt)
-        #     ckpt, _, _ = train(train_dataset, 1, 0.0001, 8, model)
-        #     ckpts.append(ckpt)
-        # for ckpt, p in zip(ckpts, people):
-        #     model.load_state_dict(ckpt)
-        for p in people:
+        for p in ['he']:
+            model.load_state_dict(ckpt_start)
+            p_except = [i for i in people if i != p]
+
+            # train_dataset = NoisyCleanSet(['json/noise_train_gt.json', 'json/dev.json', 'json/noise_train_imu.json'],
+            #                               person=p_except, time_domain=time_domain, simulation=True, text=False)
+            # ckpt, _, _ = train(train_dataset, 5, 0.0001, 16, model)
+
+            train_dataset = NoisyCleanSet(['json/noise_train_gt.json', 'json/noise_train_wav.json', 'json/noise_train_imu.json'],
+                                         person=p_except, time_domain=time_domain, simulation=False, text=False)
+            ckpt, _, _ = train(train_dataset, 5, 0.0002, 16, model)
+
+            train_dataset = NoisyCleanSet(['json/train_gt.json', 'json/dev.json', 'json/train_imu.json'],
+                                          person=[p], time_domain=time_domain, simulation=True, text=False)
+            model.load_state_dict(ckpt)
+            ckpt, _, _ = train(train_dataset, 2, 0.0001, 8, model)
+            ckpts.append(ckpt)
+        for ckpt, p in zip(ckpts, ['he']):
+            model.load_state_dict(ckpt)
             test_dataset = NoisyCleanSet(['json/noise_gt.json', 'json/noise_wav.json', 'json/noise_imu.json'],
                                          person=[p], time_domain=time_domain, simulation=False, text=True)
             metric = inference(test_dataset, 4, model)
