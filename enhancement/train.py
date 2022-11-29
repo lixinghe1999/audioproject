35ae57400a77baf703b57f0af3a63f67f66bbb1b
lixinghe1999 <helixing99@gmail.com>
2022-11-29 19:06:08 +0800

    train WER with RIR

--- enhancement/train.py
+++ enhancement/train.py
@@ -112,8 +112,8 @@ if __name__ == "__main__":
         BATCH_SIZE = 64
         lr = 0.0001
         EPOCH = 20
-        dataset = NoisyCleanSet(['json/train.json', 'json/all_noise.json'], time_domain=time_domain, simulation=True,
-                                ratio=1, rir=None)
+        # dataset = NoisyCleanSet(['json/train.json', 'json/all_noise.json'], time_domain=time_domain, simulation=True,
+        #                         ratio=1, rir=None)
         with open('json/EMSB.json', 'r') as f:
             data = json.load(f)
             person = data.keys()
