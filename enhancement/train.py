be782deba44d5b65bc3eff42bab4699ea773da4f
lixinghe1999 <helixing99@gmail.com>
2022-11-30 10:19:35 +0800

    train VibVoice with EMSB

--- enhancement/train.py
+++ enhancement/train.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
         # test_dataset = torch.utils.data.ConcatDataset([test_dataset1, test_dataset2])
 
             model.load_state_dict(ckpt)
-            ckpt, loss_curve, metric_best = train([train_dataset, test_dataset], EPOCH, lr, BATCH_SIZE, model, discriminatorme=None)
+            ckpt, loss_curve, metric_best = train([train_dataset, test_dataset], EPOCH, lr, BATCH_SIZE, model, discriminator=None)
 
         # Optional Micro-benchmark
         model.load_state_dict(ckpt)
