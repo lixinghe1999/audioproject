b250d08d99dfd67d83037269cbfeab80e2691380
lixinghe1999 <helixing99@gmail.com>
2022-12-03 20:03:09 +0800

    train SEANet

--- enhancement/train.py
+++ enhancement/train.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     elif args.mode == 1:
         # This script is for model fine-tune on self-collected dataset, by default-with all noises
         BATCH_SIZE = 16
-        lr = 0.0001
+        lr = 0.00001
         EPOCH = 30
 
         ckpt_dir = 'pretrain/seanet'
@@ -157,7 +157,7 @@ if __name__ == "__main__":
             # train_dataset = torch.utils.data.ConcatDataset([train_dataset, train_dataset2])
             # test_dataset = torch.utils.data.ConcatDataset([test_dataset, test_dataset2])
 
-            #model.load_state_dict(ckpt)
+            model.load_state_dict(ckpt)
             ckpt, loss_curve, metric_best = train([train_dataset, test_dataset], EPOCH, lr, BATCH_SIZE, model, discriminator=None)
 
         # Optional Micro-benchmark
