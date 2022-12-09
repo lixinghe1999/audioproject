26fd29080962eac82a34c07713d7997d0c250ecc
lixinghe1999 <helixing99@gmail.com>
2022-12-09 11:22:01 +0800

    very challenging case: strong speech noise

--- enhancement/train.py
+++ enhancement/train.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     parser.add_argument('--mode', action="store", type=int, default=0, required=False,
                         help='mode of processing, 0-pre train, 1-main benchmark, 2-mirco benchmark')
     args = parser.parse_args()
-    #torch.cuda.set_device(1)
+    torch.cuda.set_device(1)
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
     # model = A2net(inference=False).to(device)
     model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     discriminator = MultiScaleDiscriminator().to(device)
     time_domain = False
 
-    model = torch.nn.DataParallel(model, device_ids=[0, 1])
+    model = torch.nn.DataParallel(model, device_ids=[1])
     # discriminator = torch.nn.DataParallel(discriminator, device_ids=[0, 1])
     # discriminator = Discriminator_spectrogram().to(device)
 
