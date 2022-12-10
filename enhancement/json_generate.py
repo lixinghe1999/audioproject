722004f8ed8e42ec957510ca2da87d2a22addb3f
lixinghe1999 <helixing99@gmail.com>
2022-12-10 13:00:53 +0800

    new way of noise adding

--- enhancement/json_generate.py
+++ enhancement/json_generate.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
                         help='mode of processing, 0-audio only, 1-audio+acc')
     args = parser.parse_args()
     if args.mode == 0:
-        directory = "../dataset/roomacoustic"
+        directory = "../dataset/roomacoustic/"
         #datasets = ['dev', 'background', 'music', 'train', 'rir_noise']
         datasets = ['large_room', 'middle_room', 'small_room']
         for dataset in datasets:
