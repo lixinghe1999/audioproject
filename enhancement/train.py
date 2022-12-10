83ea08ce9e8659f134831e38552921a1c573740f
lixinghe1999 <helixing99@gmail.com>
2022-12-10 16:09:38 +0800

    test performance among room size

--- enhancement/train.py
+++ enhancement/train.py
@@ -54,7 +54,7 @@ def train(dataset, EPOCH, lr, BATCH_SIZE, model, discriminator=None, save_all=Fa
         Loss_list = []
         for i, (acc, noise, clean) in enumerate(tqdm(train_loader)):
             #loss, discrim_loss = train_SEANet(model, acc, noise, clean, optimizer, optimizer_disc, discriminator, device)
-            loss = train_vibvoice(model, acc, noise, clean, optimizer, device)
+            loss = train_fullsubnet(model, acc, noise, clean, optimizer, device)
             Loss_list.append(loss)
         mean_lost = np.mean(Loss_list)
         loss_curve.append(mean_lost)
@@ -62,7 +62,7 @@ def train(dataset, EPOCH, lr, BATCH_SIZE, model, discriminator=None, save_all=Fa
         Metric = []
         with torch.no_grad():
             for acc, noise, clean in tqdm(test_loader):
-                metric = test_vibvoice(model, acc, noise, clean, device)
+                metric = test_fullsubnet(model, acc, noise, clean, device)
                 Metric.append(metric)
         avg_metric = np.mean(np.concatenate(Metric, axis=0), axis=0)
         print(avg_metric, mean_lost)
@@ -82,10 +82,10 @@ def inference(dataset, BATCH_SIZE, model):
         for data in test_loader:
             if len(data) == 3:
                 acc, noise, clean = data
-                metric = test_vibvoice(model, acc, noise, clean, device)
+                metric = test_fullsubnet(model, acc, noise, clean, device)
             else:
                 text, acc, noise, clean = data
-                metric = test_vibvoice(model, acc, noise, clean, device, text)
+                metric = test_fullsubnet(model, acc, noise, clean, device, text)
             Metric.append(metric)
     Metric = np.concatenate(Metric, axis=0)
     return Metric
@@ -97,14 +97,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
     torch.cuda.set_device(1)
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
-    model = A2net(inference=False).to(device)
-    #model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
+    #model = A2net(inference=False).to(device)
+    model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
     # model = SEANet().to(device)
 
     #discriminator = MultiScaleDiscriminator().to(device)
     time_domain = False
 
-    # model = torch.nn.DataParallel(model, device_ids=[1])
+    model = torch.nn.DataParallel(model, device_ids=[1])
     # discriminator = torch.nn.DataParallel(discriminator, device_ids=[0, 1])
     # discriminator = Discriminator_spectrogram().to(device)
 
@@ -133,44 +133,43 @@ if __name__ == "__main__":
         # This script is for model fine-tune on self-collected dataset, by default-with all noises
         people = ["1", "2", "3", "4", "5", "6", "7", "8", "yan", "wu", "liang", "shuai", "shi", "he", "hou"]
         rir = 'json/rir_noise.json'
-        BATCH_SIZE = 16
+        BATCH_SIZE = 8
         lr = 0.0001
         EPOCH = 20
         r = 0.8
         n = 1
-        ckpt_dir = 'pretrain/new_vibvoice'
+        ckpt_dir = 'pretrain/new_fullsubnet'
         ckpt_name = ckpt_dir + '/' + sorted(os.listdir(ckpt_dir))[-1]
-        ckpt_name = 'pretrain/[ 2.46606997 13.79310007  3.60952407].pth'
+        # ckpt_name = 'pretrain/[ 2.46606997 13.79310007  3.60952407].pth'
         print("load checkpoint: {}".format(ckpt_name))
         ckpt_start = torch.load(ckpt_name)
         #
-        # train_dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'],
-        #                                 time_domain=time_domain, simulation=True, person=people, ratio=r,
-        #                               num_noises=n, snr=(0, 20), rir=rir)
-        # test_dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'],
-        #                                   time_domain=time_domain, simulation=True, person=people, ratio=-0.2,
-        #                              num_noises=n, snr=(0, 20), rir=rir)
-        #
-        # # extra dataset for other positions
-        # positions = ['glasses', 'vr-up', 'vr-down', 'headphone-inside', 'headphone-outside', 'cheek', 'temple', 'back', 'nose']
-        # train_dataset2 = NoisyCleanSet(['json/position_gt.json', 'json/all_noise.json', 'json/position_imu.json'],
-        #                                time_domain=time_domain, simulation=True, person=positions, ratio=r,
-        #                                num_noises=n, snr=(0, 20), rir=rir)
-        # test_dataset2 = NoisyCleanSet(['json/position_gt.json', 'json/all_noise.json', 'json/position_imu.json'],
-        #                               time_domain=time_domain, simulation=True, person=positions, ratio=-0.2,
-        #                               num_noises=n, snr=(0, 20), rir=rir)
-        #
-        # train_dataset = torch.utils.data.ConcatDataset([train_dataset, train_dataset2])
-        # test_dataset = torch.utils.data.ConcatDataset([test_dataset, test_dataset2])
-        #
-        # model.load_state_dict(ckpt_start)
-        # ckpt, loss_curve, metric_best = train([train_dataset, test_dataset], EPOCH, lr, BATCH_SIZE, model, discriminator=None)
+        train_dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'],
+                                        time_domain=time_domain, simulation=True, person=people, ratio=r,
+                                      num_noises=n, snr=(0, 20), rir=rir)
+        test_dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'],
+                                          time_domain=time_domain, simulation=True, person=people, ratio=-0.2,
+                                     num_noises=n, snr=(0, 20), rir=rir)
+
+        # extra dataset for other positions
+        positions = ['glasses', 'vr-up', 'vr-down', 'headphone-inside', 'headphone-outside', 'cheek', 'temple', 'back', 'nose']
+        train_dataset2 = NoisyCleanSet(['json/position_gt.json', 'json/all_noise.json', 'json/position_imu.json'],
+                                       time_domain=time_domain, simulation=True, person=positions, ratio=r,
+                                       num_noises=n, snr=(0, 20), rir=rir)
+        test_dataset2 = NoisyCleanSet(['json/position_gt.json', 'json/all_noise.json', 'json/position_imu.json'],
+                                      time_domain=time_domain, simulation=True, person=positions, ratio=-0.2,
+                                      num_noises=n, snr=(0, 20), rir=rir)
+
+        train_dataset = torch.utils.data.ConcatDataset([train_dataset, train_dataset2])
+        test_dataset = torch.utils.data.ConcatDataset([test_dataset, test_dataset2])
+
+        model.load_state_dict(ckpt_start)
+        ckpt, loss_curve, metric_best = train([train_dataset, test_dataset], EPOCH, lr, BATCH_SIZE, model, discriminator=None)
 
         # Optional Micro-benchmark
-        model.load_state_dict(ckpt_start)
+        model.load_state_dict(ckpt)
         rirs = ['json/smallroom.json', 'json/mediumroom.json', 'json/largeroom.json']
-        rirs_earphone = ['../dataset/roomacoustic/small.wav', '../dataset/roomacoustic/middle.wav', '../dataset/roomacoustic/large.wav']
-        for rir, rir_earphone in zip(rirs, rirs_earphone):
+        for rir in rirs:
             dataset = NoisyCleanSet(['json/train_gt.json', 'json/all_noise.json', 'json/train_imu.json'],
                                     person=people, time_domain=time_domain, simulation=True, ratio=-0.2,
                                     rir=rir, rir_earphone=None)
