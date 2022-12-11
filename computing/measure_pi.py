e61cb36b46c6099fef3c3aeb4b905230df844921
lixinghe1999 <helixing99@gmail.com>
2022-12-11 15:18:19 +0800

    deploy quantization on pi

--- computing/measure_pi.py
+++ computing/measure_pi.py
@@ -30,5 +30,5 @@ def inference_torch(quant_model, sample_input):
 if __name__ == "__main__":
     #sample_input = np.random.random((1, 1, 512, 300))
     #print(inference_onnx('identification.onnx', sample_input))
-    sample_input = torch.randint(0, 255, (1, 1, 512, 300), dtype=torch.int8)
+    sample_input = torch.randint(0, 255, (1, 1, 512, 300), dtype=torch.uint8)
     print(inference_torch('identification_quant.pth', sample_input))
