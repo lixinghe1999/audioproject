ee1b363a45b5edb7b40c19b26324cd317c04e78e
lixinghe1999 <helixing99@gmail.com>
2022-12-13 16:55:36 +0800

    train fullsubnet with rir + drop_band

--- enhancement/train.py
+++ enhancement/train.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
 
     if args.mode == 0:
         # This script is for model pre-training on LibriSpeech
-        BATCH_SIZE = 16
+        BATCH_SIZE = 32
         lr = 0.001
         EPOCH = 20
         dataset = NoisyCleanSet(['json/train.json', 'json/all_noise.json'], time_domain=time_domain, simulation=True,
