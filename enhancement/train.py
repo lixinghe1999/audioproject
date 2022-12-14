3a49a45712d0f66cd018b13ec2dfd8eea36e4c64
lixinghe1999 <helixing99@gmail.com>
2022-12-14 09:44:11 +0800

    train fullsubnet with rir + drop_band

--- enhancement/train.py
+++ enhancement/train.py
@@ -209,20 +209,20 @@ if __name__ == "__main__":
         ckpt_start = torch.load(ckpt_name)
         people = ["hou", "1", "2", "3", "4", "5", "6", "7", "8", "yan", "wu", "liang", "shuai", "shi", "he"]
         ckpts = []
-        # for p in people:
-        #     model.load_state_dict(ckpt_start)
-        #     p_except = [i for i in people if i != p]
-        #     train_dataset = NoisyCleanSet(['json/noise_train_gt.json', 'json/noise_train_wav.json', 'json/noise_train_imu.json'],
-        #                                  person=people, time_domain=time_domain, simulation=False, text=False)
-        #     ckpt, _, _ = train(train_dataset, 5, 0.0001, 16, model)
+        for p in ['he']:
+            model.load_state_dict(ckpt_start)
+            p_except = [i for i in people if i != p]
+            train_dataset = NoisyCleanSet(['json/noise_train_gt.json', 'json/noise_train_wav.json', 'json/noise_train_imu.json'],
+                                         person=p_except, time_domain=time_domain, simulation=False, text=False)
+            ckpt, _, _ = train(train_dataset, 1, 0.0001, 16, model)
         #
         #     train_dataset = NoisyCleanSet(['json/train_gt.json', 'json/dev.json', 'json/train_imu.json'],
         #                                   person=[p], time_domain=time_domain, simulation=True,
         #                                   rir=None, text=False, snr=(0, 20))
         #     ckpt, _, _ = train(train_dataset, 2, 0.0001, 16, model)
-        #     ckpts.append(ckpt)
-        for p in people:
-            model.load_state_dict(ckpt_start)
+            ckpts.append(ckpt)
+        for ckpt, p in zip(ckpts,  ['he']):
+            model.load_state_dict(ckpt)
             test_dataset = NoisyCleanSet(['json/noise_gt.json', 'json/noise_wav.json', 'json/noise_imu.json'],
                                          person=[p], time_domain=time_domain, simulation=False, text=True)
             metric = inference(test_dataset, 4, model)
