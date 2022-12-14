0df5788a5f0c1d47b1b194b2e2658688eba656c1
lixinghe1999 <helixing99@gmail.com>
2022-12-14 10:21:34 +0800

    train fullsubnet with rir + drop_band

--- enhancement/train.py
+++ enhancement/train.py
@@ -82,10 +82,10 @@ def inference(dataset, BATCH_SIZE, model):
         for data in test_loader:
             if len(data) == 3:
                 acc, noise, clean = data
-                metric = test_fullsubnet(model, acc, noise, clean, device)
+                metric = test_vibvoice(model, acc, noise, clean, device)
             else:
                 text, acc, noise, clean = data
-                metric = test_fullsubnet(model, acc, noise, clean, device, text)
+                metric = test_vibvoice(model, acc, noise, clean, device, text)
             Metric.append(metric)
     Metric = np.concatenate(Metric, axis=0)
     return Metric
@@ -97,14 +97,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
     torch.cuda.set_device(1)
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
-    # model = A2net().to(device)
-    model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
+    model = A2net().to(device)
+    #model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
     # model = SEANet().to(device)
 
     #discriminator = MultiScaleDiscriminator().to(device)
     time_domain = False
 
-    model = torch.nn.DataParallel(model, device_ids=[1])
+    # model = torch.nn.DataParallel(model, device_ids=[1])
     # discriminator = torch.nn.DataParallel(discriminator, device_ids=[0, 1])
     # discriminator = Discriminator_spectrogram().to(device)
 
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         EPOCH = 20
         r = 0.8
         n = 1
-        ckpt_dir = 'pretrain/fullsubnet_rir'
+        ckpt_dir = 'pretrain/vibvoice_rir'
         ckpt_name = ckpt_dir + '/' + sorted(os.listdir(ckpt_dir))[-1]
         #ckpt_name = 'pretrain/[ 2.52787821 14.42417432  3.50193285].pth'
         print("load checkpoint: {}".format(ckpt_name))
