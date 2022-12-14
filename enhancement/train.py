0fee88d8f2b0f961cfbda348f9823dd885afbac4
lixinghe1999 <helixing99@gmail.com>
2022-12-14 09:26:05 +0800

    train fullsubnet with rir + drop_band

--- enhancement/train.py
+++ enhancement/train.py
@@ -95,10 +95,10 @@ if __name__ == "__main__":
     parser.add_argument('--mode', action="store", type=int, default=0, required=False,
                         help='mode of processing, 0-pre train, 1-main benchmark, 2-mirco benchmark')
     args = parser.parse_args()
-    torch.cuda.set_device(1)
+    torch.cuda.set_device(0)
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
     model = A2net().to(device)
-    #model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
+    # model = FullSubNet(num_freqs=256, num_groups_in_drop_band=1).to(device)
     # model = SEANet().to(device)
 
     #discriminator = MultiScaleDiscriminator().to(device)
