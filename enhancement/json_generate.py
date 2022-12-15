0754235cc7ab992842b8aefdfe2a7040a27879e8
lixinghe1999 <helixing99@gmail.com>
2022-12-15 19:35:44 +0800

    test pre-trained fullsubnet

--- enhancement/json_generate.py
+++ enhancement/json_generate.py
@@ -57,7 +57,23 @@ if __name__ == "__main__":
                     if file_name[-3:] in ['wav', 'lac']:
                         audio_files.append([os.path.join(path, file_name), torchaudio.info(os.path.join(path, file_name)).num_frames])
             json.dump(audio_files, open('json/' + dataset_name + '.json', 'w'), indent=4)
-    elif args.mode == 1:
+    if args.mode == 1:
+        directory = "../dataset/DNS-Challenge/"
+        datasets = ['test_set/synthetic/with_reverb/noisy',
+                    'test_set/synthetic/with_reverb/clean']
+        for dataset in datasets:
+            audio_files = []
+            g = os.walk(directory + dataset)
+            dataset_name = 'DNS' + dataset.split('/')[-1]
+            print(dataset_name)
+            for path, dir_list, file_list in g:
+                file_list = sorted(file_list, key=lambda x: x.split('_')[-1])
+                for file_name in file_list:
+                    if file_name[-3:] in ['wav', 'lac']:
+                        audio_files.append(
+                            [os.path.join(path, file_name), torchaudio.info(os.path.join(path, file_name)).num_frames])
+            json.dump(audio_files, open('json/' + dataset_name + '.json', 'w'), indent=4)
+    elif args.mode == 2:
         directory = '../dataset/our'
         person = os.listdir(directory)
         dict = {}
