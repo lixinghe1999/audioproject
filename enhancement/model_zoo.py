f32e5158084309dcab71d63bba5d33ddffec4cd7
lixinghe1999 <helixing99@gmail.com>
2022-12-17 15:30:41 +0800

    check the new framework

--- enhancement/model_zoo.py
+++ enhancement/model_zoo.py
@@ -62,12 +62,11 @@ def train_vibvoice(model, acc, noise, clean, optimizer, device='cuda'):
     # VibVoice
     noisy_mag = noisy_mag.to(device=device)
     clean_mag = clean_mag.to(device=device)
-    print(noisy_mag.mean(), clean_mag.mean())
     clean_mag = torch.unsqueeze(clean_mag[:, 1:257, 1:], 1)
     clean, acc = model(noisy_mag, acc)
 
     loss = Spectral_Loss(clean, clean_mag)
-    loss += 0.5 * F.mse_loss(acc, clean_mag[:, :, :32, :])
+    loss += 0.1 * F.mse_loss(acc, clean_mag[:, :, :32, :])
     loss.backward()
 
     for name, param in model.named_parameters():
