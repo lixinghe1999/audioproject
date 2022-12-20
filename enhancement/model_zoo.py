cdddf1be893c12e5c7d4c5bfacbfb10784983381
lixinghe1999 <helixing99@gmail.com>
2022-12-20 12:32:41 +0800

    use voicefilter-lite

--- enhancement/model_zoo.py
+++ enhancement/model_zoo.py
@@ -108,9 +108,7 @@ def test_vibvoice(model, acc, noise, clean, device='cuda', text=None, data=False
 
     predict, acc = model(noisy_mag, acc)
     predict = predict.squeeze(1).cpu()
-    print(predict.shape)
     predict = F.pad(predict, (1, 0, 1, 321 - 257))
-    print(predict.shape)
     predict = istft((predict, noisy_phase), 640, 320, 640, input_type="mag_phase").numpy()
     clean = clean.numpy()
     if data:
