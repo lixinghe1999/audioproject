3fc64e0e135c5fc9489523112e054af8725741b7
lixinghe1999 <helixing99@gmail.com>
2022-12-22 12:19:02 +0800

    train sudormrf

--- enhancement/train.py
+++ enhancement/train.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
 
     if args.mode == 0:
         # This script is for model pre-training on LibriSpeech
-        BATCH_SIZE = 32
+        BATCH_SIZE = 64
         lr = 0.0001
         EPOCH = 20
         # ckpt = torch.load('voiceSplit-trained-with-MSE-GE2E-Seungwonpark-best_checkpoint.pt')
