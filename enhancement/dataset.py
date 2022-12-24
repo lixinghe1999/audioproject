c369af47e90df1c08b94ef9c4557b9cad4146d6d
lixinghe1999 <helixing99@gmail.com>
2022-12-24 10:43:28 +0800

    train sudormrf

--- enhancement/dataset.py
+++ enhancement/dataset.py
@@ -92,7 +92,7 @@ class NoiseDataset:
         remaining_length = self.target_length
 
         while remaining_length > 0:
-            noise_file, info = np.random.choice(self.files)
+            noise_file, info = self.files[np.random.randint(0, self.__len__())]
             noise_new_added, sr = librosa.load(noise_file, sr=self.sr)
             noise_y = np.append(noise_y, noise_new_added)
             remaining_length -= len(noise_new_added)
