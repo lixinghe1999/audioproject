ac11eb644c47f329e087501d60e4f82405dd027a
lixinghe1999 <helixing99@gmail.com>
2023-01-02 17:09:35 +0800

    update config

---
+++ event/utils/datasets/esc50.py
@@ -0,0 +1,121 @@
+import os
+import warnings
+import multiprocessing as mp
+
+import tqdm
+import librosa
+
+import numpy as np
+import pandas as pd
+
+import torch.utils.data as td
+
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Union
+from typing import Optional
+
+
+class ESC50(td.Dataset):
+
+    def __init__(self,
+                 root: str,
+                 sample_rate: int = 22050,
+                 train: bool = True,
+                 fold: Optional[int] = None,
+                 transform_audio=None,
+                 target_transform=None,
+                 **_):
+
+        super(ESC50, self).__init__()
+
+        self.sample_rate = sample_rate
+
+        meta = self.load_meta(os.path.join(root, 'meta', 'esc50.csv'))
+
+        if fold is None:
+            fold = 5
+
+        self.folds_to_load = set(meta['fold'])
+
+        if fold not in self.folds_to_load:
+            raise ValueError(f'fold {fold} does not exist')
+        self.train = train
+        self.transform = transform_audio
+
+        if self.train:
+            self.folds_to_load -= {fold}
+        else:
+            self.folds_to_load -= self.folds_to_load - {fold}
+
+        self.data = list()
+        self.load_data(meta, os.path.join(root, 'audio'))
+        # self.indices = list(self.data.keys())
+        #
+        self.class_idx_to_label = dict()
+        for row in self.data:
+            idx = row['target']
+            label = row['category']
+            self.class_idx_to_label[idx] = label
+        self.label_to_class_idx = {lb: idx for idx, lb in self.class_idx_to_label.items()}
+
+        # self.target_transform = target_transform
+
+    @staticmethod
+    def load_meta(path_to_csv: str) -> pd.DataFrame:
+        meta = pd.read_csv(path_to_csv)
+
+        return meta
+
+    @staticmethod
+    def _load_worker(idx: int, filename: str, sample_rate: Optional[int] = None) -> Tuple[int, int, np.ndarray]:
+        wav, sample_rate = librosa.load(filename, sr=sample_rate, mono=True)
+
+        if wav.ndim == 1:
+            wav = wav[:, np.newaxis]
+
+        wav = wav.T * 32768.0
+
+        return idx, sample_rate, wav.astype(np.float32)
+
+    def load_data(self, meta: pd.DataFrame, base_path: str):
+        items_to_load = dict()
+
+        for idx, row in meta.iterrows():
+            if row['fold'] in self.folds_to_load:
+                items_to_load[idx] = os.path.join(base_path, row['filename']), self.sample_rate
+
+        items_to_load = [(idx, path, sample_rate) for idx, (path, sample_rate) in items_to_load.items()]
+        for item in items_to_load:
+            idx, path, sample_rate = item
+            row = meta.loc[idx]
+            self.data.append({
+                        'audio': path,
+                        'sample_rate': sample_rate,
+                        'target': row['target'],
+                        'category': row['category'].replace('_', ' '),
+                        'fold': row['fold'],
+                        'esc10': row['esc10']
+                    })
+        return items_to_load
+
+    def __getitem__(self, index: int) -> Tuple[np.ndarray, str]:
+        if not (0 <= index < len(self)):
+            raise IndexError
+
+        sample = self.data[index]
+        filename: str = sample['audio']
+        audio, sample_rate = librosa.load(filename, sr=sample['sample_rate'], mono=True)
+        if audio.ndim == 1:
+            audio = audio[:, np.newaxis]
+        audio = (audio.T * 32768.0).astype(np.float32)
+        target: str = self.data[index]['category']
+
+        if self.transform is not None:
+            audio = self.transform(audio)
+        return audio, target
+
+    def __len__(self) -> int:
+        return len(self.data)
---
+++ event/zero-shot.py
@@ -0,0 +1,83 @@
+import torch
+from model import AudioCLIP
+from utils.transforms import ToTensor1D
+from utils.datasets.esc50 import ESC50
+def zero_shot_eval(logits_audio_text, ):
+    print('\t\tFilename, Audio\t\t\tTextual Label (Confidence)', end='\n\n')
+
+    # calculate model confidence
+    confidence = logits_audio_text.softmax(dim=1)
+    for audio_idx in range(len(paths_to_audio)):
+        # acquire Top-3 most similar results
+        conf_values, ids = confidence[audio_idx].topk(3)
+
+        # format output strings
+        query = f'{os.path.basename(paths_to_audio[audio_idx]):>30s} ->\t\t'
+        results = ', '.join([f'{LABELS[i]:>15s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
+
+        print(query + results)
+
+    print('\t\tTextual Label\t\tFilename, Audio (Confidence)', end='\n\n')
+
+    # calculate model confidence
+    confidence = logits_audio_text.softmax(dim=0)
+    for label_idx in range(len(LABELS)):
+        # acquire Top-2 most similar results
+        conf_values, ids = confidence[:, label_idx].topk(2)
+
+        # format output strings
+        query = f'{LABELS[label_idx]:>25s} ->\t\t'
+        results = ', '.join(
+            [f'{os.path.basename(paths_to_audio[i]):>30s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
+
+        print(query + results)
+if __name__ == "__main__":
+    torch.set_grad_enabled(False)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    MODEL_FILENAME = 'AudioCLIP-Full-Training.pt'
+    # derived from ESResNeXt
+    SAMPLE_RATE = 44100
+
+    model = AudioCLIP(pretrained=f'assets/{MODEL_FILENAME}').to(device)
+    audio_transforms = ToTensor1D()
+    dataset = ESC50('../dataset/ESC50', train=False, transform_audio=audio_transforms, sample_rate=SAMPLE_RATE)
+    loader = torch.utils.data.DataLoader(dataset=dataset, num_workers=4, batch_size=6, shuffle=True)
+
+    with torch.no_grad():
+        for sample in loader:
+            audio, text = sample
+            audio = audio.to(device)
+            print(audio.shape, text)
+
+            ((audio_features, _, _), _), _ = model(audio=audio, batch_indices=
+            torch.arange(audio.shape[0], dtype=torch.int64, device=device))
+
+            ((_, _, text_features), _), _ = model(text=[
+                        [dataset.class_idx_to_label[class_idx]]
+                        for class_idx in sorted(eval_loader.dataset.class_idx_to_label.keys())
+                    ], batch_indices=torch.arange(
+                        len(dataset.class_idx_to_label), dtype=torch.int64, device=device
+                    ))
+            print(audio_features.shape, text_features.shape)
+            audio_features = audio_features.unsqueeze(1)
+            text_features = text_features.unsqueeze(1).transpose(0, 1)
+            print(audio_features.shape, text_features.shape)
+            logit_scale_at = torch.clamp(model.logit_scale_at.exp(), min=1.0, max=100.0)
+            print(logit_scale_at)
+            y_pred = (logit_scale_at * audio_features @ text_features.transpose(-1, -2)).squeeze(1)
+            print(y_pred)
+            y = torch.zeros(
+                audio.shape[0], len(dataset.class_idx_to_label), dtype=torch.int8, device=device
+            )
+            for item_idx, labels in enumerate(text):
+                class_ids = list(sorted([
+                    dataset.label_to_class_idx[lb] for lb in labels]))
+                y[item_idx][class_ids] = 1
+            break
+            if model.multilabel:
+                y_pred = torch.sigmoid(y_pred / logit_scale_at - 0.5)
+            else:
+                y_pred = torch.softmax(y_pred, dim=-1)
+                y = y.argmax(dim=-1)
+
