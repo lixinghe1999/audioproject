0fa7e6ddea60bbe8d423100b005eb89845dfb026
lixinghe1999 <helixing99@gmail.com>
2023-01-02 19:14:42 +0800

    update config

--- event/zero-shot.py
+++ event/zero-shot.py
@@ -2,18 +2,26 @@ import torch
 from model import AudioCLIP
 from utils.transforms import ToTensor1D
 from utils.datasets.esc50 import ESC50
-def zero_shot_eval(logits_audio_text, y, class_idx_to_label):
+def zero_shot_eval(logits_audio_text, y, class_idx_to_label, print_result=False):
     # calculate model confidence
     num_audio = logits_audio_text.shape[0]
     confidence = logits_audio_text.softmax(dim=0)
+    top1_a = 0
+    top3_a = 0
     for audio_idx in range(num_audio):
         # acquire Top-3 most similar results
         conf_values, ids = confidence[audio_idx].topk(3)
+        gt = y[audio_idx].item()
+        if gt == ids[0]:
+            top1_a += 1
+        if (gt == ids).any():
+            top3_a += 1
         # format output strings
-        query = class_idx_to_label[y[audio_idx].item()]
-        results = ', '.join([f'{class_idx_to_label[i.item()]:>15s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
-
-        print(query + ', ' + results)
+        if print_result:
+            query = class_idx_to_label[gt]
+            results = ', '.join([f'{class_idx_to_label[i.item()]:>15s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
+            print(query + ', ' + results)
+    return top1_a/num_audio, top3_a/num_audio
 def collate_fn(batch):
     batch_audio, batch_text = zip(*batch)
 
@@ -21,12 +29,10 @@ def collate_fn(batch):
         batch_audio = torch.stack(batch_audio)
     else:
         batch_audio = None
-
     if not all(text is None for text in batch_text):
         batch_text = [idx for idx in batch_text]
     else:
         batch_text = None
-
     return batch_audio, batch_text
 
 if __name__ == "__main__":
@@ -47,8 +53,6 @@ if __name__ == "__main__":
         for sample in loader:
             audio, text = sample
             audio = audio.to(device)
-            print(audio.shape, text)
-
             ((audio_features, _, _), _), _ = model(audio=audio, batch_indices=
             torch.arange(audio.shape[0], dtype=torch.int64, device=device))
 
@@ -58,12 +62,10 @@ if __name__ == "__main__":
                     ], batch_indices=torch.arange(
                         len(dataset.class_idx_to_label), dtype=torch.int64, device=device
                     ))
-            print(audio_features.shape, text_features.shape)
             audio_features = audio_features.unsqueeze(1)
             text_features = text_features.unsqueeze(1).transpose(0, 1)
-            print(audio_features.shape, text_features.shape)
             logit_scale_at = torch.clamp(model.logit_scale_at.exp(), min=1.0, max=100.0)
-            print(logit_scale_at)
+
             y_pred = (logit_scale_at * audio_features @ text_features.transpose(-1, -2)).squeeze(1)
             y = torch.zeros(
                 audio.shape[0], len(dataset.class_idx_to_label), dtype=torch.int8, device=device
@@ -72,11 +74,9 @@ if __name__ == "__main__":
                 class_ids = list(sorted([
                     dataset.label_to_class_idx[lb] for lb in labels]))
                 y[item_idx][class_ids] = 1
-            print(y)
             y_pred = torch.softmax(y_pred, dim=-1).cpu()
             y = y.argmax(dim=-1)
-            print(y_pred.shape)
-            print(y.shape)
-            zero_shot_eval(y_pred, y, dataset.class_idx_to_label)
+            top1, top3 = zero_shot_eval(y_pred, y, dataset.class_idx_to_label, print_result=True)
+            print(top1, top3)
             break
 
