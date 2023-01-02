c5c788c225266faf204500534419a10617ac8f9e
lixinghe1999 <helixing99@gmail.com>
2023-01-02 17:15:13 +0800

    update config

--- event/zero-shot.py
+++ event/zero-shot.py
@@ -35,6 +35,7 @@ if __name__ == "__main__":
     torch.set_grad_enabled(False)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(1)
     MODEL_FILENAME = 'AudioCLIP-Full-Training.pt'
     # derived from ESResNeXt
     SAMPLE_RATE = 44100
