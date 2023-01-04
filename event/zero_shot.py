751e92ccc4efb7c2ba5b6d8b9b211cc427d7980f
lixinghe1999 <helixing99@gmail.com>
2023-01-04 12:20:21 +0800

    update config

--- event/zero_shot.py
+++ event/zero_shot.py
@@ -67,11 +67,10 @@ def collate_fn(batch):
     return batch_audio, batch_image, batch_text
 
 if __name__ == "__main__":
-    torch.set_grad_enabled(False)
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(0)
-    MODEL_FILENAME = 'ESC50_Multimodal-Audio_ACLIP-CV1_ACLIP-CV1_performance=0.9550.pt'
+    #MODEL_FILENAME = 'ESC50_Multimodal-Audio_ACLIP-CV1_ACLIP-CV1_performance=0.9550.pt'
+    MODEL_FILENAME = '[0.87, 0.965].pth'
     # derived from ESResNeXt
     SAMPLE_RATE = 44100
 
