af1313aa16c38ef8a6ad607c3a13595358c3772a
lixinghe1999 <helixing99@gmail.com>
2023-01-04 20:15:51 +0800

    update config

--- event/utils/datasets/split_dataset.py
+++ event/utils/datasets/split_dataset.py
@@ -9,7 +9,7 @@ def split_dataset(dataset, num):
     type_list = []
     dataset_list = []
     for i in range(num):
-        d = Subset(dataset, [i, (i+1) * int(len(dataset) / num)])
+        d = Subset(dataset, range(i, (i+1) * int(len(dataset) / num)))
         dataset_list.append(d)
         types = []
         for sample in dataset.data[i: (i+1) * int(len(dataset) / num)]:
