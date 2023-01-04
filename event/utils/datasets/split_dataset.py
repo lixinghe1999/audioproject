ac43af2eb16b913a79091b8bbe49c018b27aca31
lixinghe1999 <helixing99@gmail.com>
2023-01-04 20:09:47 +0800

    update config

--- event/utils/datasets/split_dataset.py
+++ event/utils/datasets/split_dataset.py
@@ -1,4 +1,4 @@
-from torch.utils.data import random_split
+from torch.utils.data import random_split, Subset
 
 def split_dataset(dataset, num):
     '''
@@ -7,10 +7,12 @@ def split_dataset(dataset, num):
     :return: [dataset_list], [corresponding type]
     '''
     type_list = []
-    dataset_list = random_split(dataset, [int(len(dataset) / num)] * num)
-    for d in dataset_list:
+    dataset_list = []
+    for i in range(len(num)):
+        dataset = Subset(dataset, [i, (i+1) * int(len(dataset) / num)])
+        dataset_list.append(dataset)
         types = []
-        for sample in d.data:
+        for sample in dataset.data:
             types.append(sample['target'])
         type_list.append(types)
     return dataset_list, type_list
