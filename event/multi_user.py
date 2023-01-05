8154d174d9f8232edf32016c17e2556bad6de099
lixinghe1999 <helixing99@gmail.com>
2023-01-05 10:13:32 +0800

    update config

--- event/multi_user.py
+++ event/multi_user.py
@@ -4,38 +4,26 @@ from utils.datasets.esc50 import ESC50
 from utils.datasets.split_dataset import split_dataset, split_dataset_type
 from utils.train import training_step, prepare_model, collate_fn, zero_shot_eval, eval_step
 import numpy as np
-from tqdm import tqdm
-
-if __name__ == "__main__":
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    torch.cuda.set_device(1)
-    MODEL_FILENAME = 'AudioCLIP-Full-Training.pt'
-    # derived from ESResNeXt
-    SAMPLE_RATE = 44100
+def fine_tune(train_dataset, test_dataset, MODEL_FILENAME, device):
     model = AudioCLIP(pretrained=f'assets/{MODEL_FILENAME}').to(device)
-    train_dataset = ESC50('../dataset/ESC50', fold=1, train=True, sample_rate=SAMPLE_RATE, few_shot=None)
-    test_dataset = ESC50('../dataset/ESC50', fold=1, train=False, sample_rate=SAMPLE_RATE)
-
-    # train_dataset_list, type_list = split_dataset(train_dataset, 10)
+    model, param_groups = prepare_model(model)
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, num_workers=4, batch_size=16, shuffle=True,
                                                drop_last=False, collate_fn=collate_fn)
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, num_workers=4, batch_size=16, shuffle=False,
-                                         drop_last=False, collate_fn=collate_fn)
-    model, param_groups = prepare_model(model)
+                                              drop_last=False, collate_fn=collate_fn)
     optimizer = torch.optim.SGD(param_groups, **{**{
-     "lr": 5e-5, "momentum": 0.9, "nesterov": True, "weight_decay": 5e-4}, **{'lr': 5e-5 }})
+        "lr": 5e-5, "momentum": 0.9, "nesterov": True, "weight_decay": 5e-4}, **{'lr': 5e-5}})
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.96)
 
-
-    loss_best = 1
+    loss_best = 2
     ((_, _, text_features), _), _ = model(text=[
         [test_dataset.class_idx_to_label[class_idx]]
         for class_idx in sorted(test_dataset.class_idx_to_label.keys())
     ], batch_indices=torch.arange(len(test_dataset.class_idx_to_label), dtype=torch.int64, device=device))
     text_features = text_features.unsqueeze(1).transpose(0, 1)
-    for e in range(1):
+    for e in range(10):
         Loss_list = []
-        for i, batch in tqdm(enumerate(train_loader)):
+        for i, batch in enumerate(train_loader):
             loss = training_step(model, batch, optimizer, device)
             if i % 200 == 0 and i != 0:
                 print(loss)
@@ -49,9 +37,26 @@ if __name__ == "__main__":
             acc_1 += top1
             acc_3 += top3
         metric = [acc_1 / len(test_loader), acc_3 / len(test_loader)]
-        print(metric, mean_lost)
         if mean_lost < loss_best:
+            print(metric, mean_lost)
             ckpt_best = model.state_dict()
             loss_best = mean_lost
             metric_best = metric
-            torch.save(ckpt_best, 'assets/' + str(metric_best) + '.pt')
+    print('the final result:', metric_best)
+    torch.save(ckpt_best, 'assets/' + str(metric_best) + '.pt')
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(1)
+    MODEL_FILENAME = 'AudioCLIP-Full-Training.pt'
+    # derived from ESResNeXt
+    SAMPLE_RATE = 44100
+
+    train_dataset = ESC50('../dataset/ESC50', fold=1, train=True, sample_rate=SAMPLE_RATE, few_shot=None)
+    test_dataset = ESC50('../dataset/ESC50', fold=1, train=False, sample_rate=SAMPLE_RATE)
+
+    num_users = 10
+    train_dataset_list, type_list = split_dataset(train_dataset, num_users)
+    test_dataset_list = split_dataset_type(test_dataset, type_list)
+    for tr, te in zip(train_dataset_list, test_dataset_list):
+        print(len(tr), len(te))
+
