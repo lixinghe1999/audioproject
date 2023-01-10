ab6f7619ef5e357858dda5098e15bfd1415d2118
lixinghe1999 <helixing99@gmail.com>
2023-01-10 14:16:59 +0800

    add ASR remove AudioClip

--- enhancement/model_zoo.py
+++ enhancement/model_zoo.py
@@ -167,7 +167,7 @@ def train_fullsubnet(model, acc, noise, clean, optimizer, device='cuda'):
     _, _, clean_real, clean_imag = stft(clean, 512, 256, 512)
     cIRM = build_complex_ideal_ratio_mask(noisy_real.unsqueeze(1), noisy_imag.unsqueeze(1),
                                           clean_real.unsqueeze(1), clean_imag.unsqueeze(1))  # [B, F, T, 2]
-    cIRM = drop_band(cIRM, 2)
+    # cIRM = drop_band(cIRM, 2)
 
     noisy_mag = noisy_mag.unsqueeze(1)
     cRM = model(noisy_mag)
