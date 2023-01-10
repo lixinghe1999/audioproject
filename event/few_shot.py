a7b13905910b243218126378cd081df86198722e
lixinghe1999 <helixing99@gmail.com>
2023-01-10 15:17:52 +0800

    add ASR remove AudioClip

--- event/few_shot.py
+++ event/few_shot.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     model = AudioCLIP(pretrained=f'assets/{MODEL_FILENAME}').to(device)
     # train_dataset = ESC50('../dataset/ESC50', fold=1, train=True, sample_rate=SAMPLE_RATE, few_shot=1)
     # test_dataset = ESC50('../dataset/ESC50', fold=1, train=False, sample_rate=SAMPLE_RATE)
-    train_dataset = FSD50K('../dataset/FSD50K', train=True, sample_rate=SAMPLE_RATE)
+    train_dataset = FSD50K('../dataset/FSD50K', train=True, sample_rate=SAMPLE_RATE, few_shot=5)
     test_dataset = FSD50K('../dataset/FSD50K', train=False, sample_rate=SAMPLE_RATE)
 
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, num_workers=4, batch_size=16, shuffle=True,
