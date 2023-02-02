2d1218e43a0258e825e450e3f2745bb89b710916
lixinghe1999 <helixing99@gmail.com>
2023-02-02 16:29:38 +0800

    add ASR remove AudioClip

--- event/key_frame.py
+++ event/key_frame.py
@@ -5,6 +5,7 @@ import torch
 from model import AudioCLIP
 from utils.train import collate_fn, zero_shot_eval, eval_step
 import warnings
+from tqdm import tqdm
 warnings.filterwarnings("ignore")
 # remove annoying librosa warning
 
@@ -21,16 +22,15 @@ if __name__ == "__main__":
     acc_a = []
     acc_i = []
     save = {'audio':[], 'image':[], 'text':[], 'y':[]}
-
-    with torch.no_grad():
-        ((_, _, text_features), _), _ = model(text=[
-            [dataset.class_idx_to_label[class_idx]]
-            for class_idx in sorted(dataset.class_idx_to_label.keys())
-        ], batch_indices=torch.arange(len(dataset.class_idx_to_label), dtype=torch.int64, device=device))
-        text_features = text_features.unsqueeze(1).transpose(0, 1)
-
-    for batch in loader:
-        y_pred_a, y_pred_i, y = eval_step(batch, model, dataset, device, save=save, text_features=text_features)
+    # with torch.no_grad():
+    #     ((_, _, text_features), _), _ = model(text=[
+    #         [dataset.class_idx_to_label[class_idx]]
+    #         for class_idx in sorted(dataset.class_idx_to_label.keys())
+    #     ], batch_indices=torch.arange(len(dataset.class_idx_to_label), dtype=torch.int64, device=device))
+    #     text_features = text_features.unsqueeze(1).transpose(0, 1)
+    # print('finish text embedding extraction')
+    for batch in tqdm(loader):
+        y_pred_a, y_pred_i, y = eval_step(batch, model, dataset, device, save=save, text_features=None)
         top1, top3 = zero_shot_eval(y_pred_a, y)
         print(top1, top3)
         acc_a.append([top1, top3])
