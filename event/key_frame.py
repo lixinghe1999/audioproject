0b120a38088246b8f33df1ad13f56d24dbbdebce
lixinghe1999 <helixing99@gmail.com>
2023-02-02 18:31:29 +0800

    add ASR remove AudioClip

--- event/key_frame.py
+++ event/key_frame.py
@@ -17,21 +17,20 @@ if __name__ == "__main__":
     model = AudioCLIP(pretrained=f'assets/{MODEL_FILENAME}').to(device)
     # dataset = EPIC_Kitchen()
     dataset = UCF101()
-    loader = torch.utils.data.DataLoader(dataset=dataset, num_workers=8, batch_size=16, shuffle=True,
+    loader = torch.utils.data.DataLoader(dataset=dataset, num_workers=8, batch_size=32, shuffle=True,
                                          drop_last=True, collate_fn=collate_fn)
     acc_a = []
     acc_i = []
     save = {'audio':[], 'image':[], 'text':[], 'y':[]}
-    with torch.no_grad():
-        ((_, _, text_features), _), _ = model(text=[
-            [dataset.class_idx_to_label[class_idx]]
-            for class_idx in sorted(dataset.class_idx_to_label.keys())
-        ], batch_indices=torch.arange(len(dataset.class_idx_to_label), dtype=torch.int64, device=device))
-        text_features = text_features.unsqueeze(1).transpose(0, 1)
-    print(text_features.shape)
-    print('finish text embedding extraction')
+    # with torch.no_grad():
+    #     ((_, _, text_features), _), _ = model(text=[
+    #         [dataset.class_idx_to_label[class_idx]]
+    #         for class_idx in sorted(dataset.class_idx_to_label.keys())
+    #     ], batch_indices=torch.arange(len(dataset.class_idx_to_label), dtype=torch.int64, device=device))
+    #     text_features = text_features.unsqueeze(1).transpose(0, 1)
+    # print('finish text embedding extraction')
     for batch in tqdm(loader):
-        y_pred_a, y_pred_i, y = eval_step(batch, model, dataset, device, save=save, text_features=text_features)
+        y_pred_a, y_pred_i, y = eval_step(batch, model, dataset, device, save=save, text_features=None)
         top1, top3 = zero_shot_eval(y_pred_a, y)
         print(top1, top3)
         acc_a.append([top1, top3])
@@ -41,4 +40,4 @@ if __name__ == "__main__":
     print(np.mean(acc_a, axis=0))
     print(np.mean(acc_i, axis=0))
     np.savez('save_embedding', audio=np.concatenate(save['audio']), image=np.concatenate(save['image']),
-            text=text_features.cpu().numpy(), y=save['y'])
+            text=text_features.squeeze(0).cpu().numpy(), y=save['y'])
