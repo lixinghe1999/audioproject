9c448c5a8d5ab1e331e0d0a3a3b23e097276e6e9
lixinghe1999 <helixing99@gmail.com>
2023-02-02 18:55:53 +0800

    add ASR remove AudioClip

--- event/key_frame.py
+++ event/key_frame.py
@@ -22,22 +22,22 @@ if __name__ == "__main__":
     acc_a = []
     acc_i = []
     save = {'audio':[], 'image':[], 'text':[], 'y':[]}
-    # with torch.no_grad():
-    #     ((_, _, text_features), _), _ = model(text=[
-    #         [dataset.class_idx_to_label[class_idx]]
-    #         for class_idx in sorted(dataset.class_idx_to_label.keys())
-    #     ], batch_indices=torch.arange(len(dataset.class_idx_to_label), dtype=torch.int64, device=device))
-    #     text_features = text_features.unsqueeze(1).transpose(0, 1)
-    # print('finish text embedding extraction')
+    with torch.no_grad():
+        ((_, _, text_features), _), _ = model(text=[
+            [dataset.class_idx_to_label[class_idx]]
+            for class_idx in sorted(dataset.class_idx_to_label.keys())
+        ], batch_indices=torch.arange(len(dataset.class_idx_to_label), dtype=torch.int64, device=device))
+        text_features = text_features.unsqueeze(1).transpose(0, 1)
+    print('finish text embedding extraction')
     for batch in tqdm(loader):
-        y_pred_a, y_pred_i, y = eval_step(batch, model, dataset, device, save=save, text_features=None)
+        y_pred_a, y_pred_i, y = eval_step(batch, model, dataset, device, save=save, text_features=text_features)
         top1, top3 = zero_shot_eval(y_pred_a, y)
-        print(top1, top3)
+        # print(top1, top3)
         acc_a.append([top1, top3])
         top1, top3 = zero_shot_eval(y_pred_i, y)
-        print(top1, top3)
+        # print(top1, top3)
         acc_i.append([top1, top3])
     print(np.mean(acc_a, axis=0))
     print(np.mean(acc_i, axis=0))
     np.savez('save_embedding', audio=np.concatenate(save['audio']), image=np.concatenate(save['image']),
-            text=np.concatenate(save['text']), y=save['y'])
+            text=text_features.squeeze(0).cpu().numpy(), y=save['y'])
