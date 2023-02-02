f38c877214d9871bf075678b9014930116c13b31
lixinghe1999 <helixing99@gmail.com>
2023-02-02 18:48:41 +0800

    add ASR remove AudioClip

--- event/utils/datasets/ucf101.py
+++ event/utils/datasets/ucf101.py
@@ -79,8 +79,7 @@ class UCF101(td.Dataset):
             audio = np.pad(audio, (0, self.length * self.sample_rate - len(audio)))
             audio = (audio * 32768.0).astype(np.float32)
         else:
-            audio = np.zeros((1, self.sample_rate * self.length))
-        print(audio.shape)
+            audio = np.zeros((self.sample_rate * self.length))
         if self.transform_audio is not None and audio is not None:
             audio = self.transform_audio(audio)
         if self.transform_image is not None and image is not None:
