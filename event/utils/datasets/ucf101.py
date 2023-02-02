f4ce71cd8fcffe7904d2a6ac2e141408ca1c5323
lixinghe1999 <helixing99@gmail.com>
2023-02-02 15:21:29 +0800

    add ASR remove AudioClip

--- event/utils/datasets/ucf101.py
+++ event/utils/datasets/ucf101.py
@@ -65,30 +65,30 @@ class UCF101(td.Dataset):
         row = self.data[index].rstrip()
         # print(row)
         fname_video = self.root + row
+        fname_audio = self.root + row[:-3] + 'wav'
         target = [row.split('/')[0]]
         vid = ffmpeg.probe(fname_video)
-        for stream in (vid['streams']):
-            if stream['codec_type'] == 'video':
-                center = stream['duration']/2
-            elif stream['codec_type'] == 'audio':
-                pass
+        center = vid['stream']['duration'] / 2
         image, _, _ = tv.io.read_video(fname_video, start_pts=center, end_pts=center, pts_unit='sec')
         image = (image[0] / 255).permute(2, 0, 1)
-        audio, sample_rate = librosa.load(fname_audio, sr=self.sample_rate, offset=center - self.length/2, duration=self.length)
-
-
-        if len(audio) >= self.length * self.sample_rate:
-            t_start = random.sample(range(len(audio) - self.length * self.sample_rate + 1), 1)[0]
-            audio = audio[t_start: t_start + self.length * self.sample_rate]
+        if os.path.isfile(fname_audio):
+            audio, sample_rate = librosa.load(fname_audio, sr=self.sample_rate, offset=center - self.length/2, duration=self.length)
+            if len(audio) >= self.length * self.sample_rate:
+                t_start = random.sample(range(len(audio) - self.length * self.sample_rate + 1), 1)[0]
+                audio = audio[t_start: t_start + self.length * self.sample_rate]
+            else:
+                audio = np.pad(audio, (0, self.length * self.sample_rate - len(audio)))
+            if audio.ndim == 1:
+                audio = audio[:, np.newaxis]
+            audio = (audio.T * 32768.0).astype(np.float32)
         else:
-            audio = np.pad(audio, (0, self.length * self.sample_rate - len(audio)))
-        if audio.ndim == 1:
-            audio = audio[:, np.newaxis]
-        audio = (audio.T * 32768.0).astype(np.float32)
-        if self.transform_audio is not None:
+            audio = None
+        if self.transform_audio is not None and audio is not None:
             audio = self.transform_audio(audio)
-        if self.transform_image is not None:
+            print(audio.shape)
+        if self.transform_image is not None and image is not None:
             image = self.transform_image(image)
+            print(image.shape)
         return audio, image, target
 
     def __len__(self) -> int:
@@ -98,13 +98,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('videos_dir', help='Directory of UCF videos with audio')
-   #parser.add_argument('output_dir', help='Directory of UCF videos with audio')
     parser.add_argument('--sample_rate', default='44100', help='Rate to resample audio')
 
     args = parser.parse_args()
-
-    # if not os.path.exists(args.output_dir):
-    #     os.mkdir(args.output_dir)
 
     for root, dirs, files in os.walk(args.videos_dir):
         for f in files:
