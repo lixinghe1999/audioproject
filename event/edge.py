8999162fda5ef7d9acf2a7fac89db87519e7ebe3
lixinghe1999 <helixing99@gmail.com>
2023-02-09 15:16:15 +0800

    audio-visual

--- event/edge.py
+++ event/edge.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore")
 def step(model, input_data, optimizers, criteria, label):
     audio, image = input_data
     # Track history only in training
-    for branch in [0, 1]:
+    for branch in [0]:
         optimizer = optimizers[branch]
         output = model(audio, image)
         # Backward
@@ -28,8 +28,9 @@ def train(train_dataset, test_dataset):
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset, num_workers=8, batch_size=16, shuffle=True,
                                                drop_last=True, pin_memory=True)
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, num_workers=4, batch_size=16, shuffle=False)
-    optimizers = [torch.optim.Adam(model.get_image_params(), lr=.0001, weight_decay=1e-4),
-                  torch.optim.Adam(model.get_audio_params(), lr=.0001, weight_decay=1e-4)]
+    # optimizers = [torch.optim.Adam(model.get_image_params(), lr=.0001, weight_decay=1e-4),
+    #               torch.optim.Adam(model.get_audio_params(), lr=.0001, weight_decay=1e-4)]
+    optimizers = [torch.optim.Adam(model.parameters(), lr=.0001, weight_decay=1e-4)]
     criteria = torch.nn.CrossEntropyLoss()
     for e in range(10):
         model.train()
