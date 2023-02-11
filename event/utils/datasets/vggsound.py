5965991126b5491a6d49819a4b2afaa3864df460
lixinghe1999 <helixing99@gmail.com>
2023-02-11 16:12:05 +0800

    audio-visual

--- event/utils/datasets/vggsound.py
+++ event/utils/datasets/vggsound.py
@@ -88,10 +88,15 @@ if __name__ == "__main__":
     def crop(data):
         idx, row = data
         name = row[0]
+
+        input_file = name + '.flac'
+        output_file = name.replace('VggSound', 'VggSound_scaled') + '.flac'
+        subprocess.call(
+            ['ffmpeg', '-i', input_file, '-ac', '1', '-ar', '16000', '-c:a', 'libmp3lame', '-q:a', '9', output_file])
         input_file = name + '.mp4'
         output_file = name.replace('VggSound', 'VggSound_scaled') + '.mp4'
         subprocess.call(
-            ['ffmpeg', '-y', '-i', input_file, '-filter:v', 'scale=480:-2', output_file])
+            ['ffmpeg', '-i', input_file, '-filter:v', 'scale=480:-2', output_file])
     def check(data):
         data_dir = '../dataset/VggSound'
         name, time, category = data
