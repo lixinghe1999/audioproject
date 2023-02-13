a7b6e9bfd85f088bcfd033f12e93637404b95f80
lixinghe1999 <helixing99@gmail.com>
2023-02-13 18:57:29 +0800

    audio-visual

--- event/vanilla.py
+++ event/vanilla.py
@@ -54,7 +54,7 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(1)
     # model = AVnet().to(device)
-    model = SingleNet(modality='A')
+    model = SingleNet(modality='A').to(device)
     dataset = VGGSound()
     len_train = int(len(dataset) * 0.8)
     len_test = len(dataset) - len_train
