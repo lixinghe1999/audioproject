44207eeaada0eb4e3a41546c5b70d17fb5a54eba
lixinghe1999 <helixing99@gmail.com>
2023-02-16 16:27:08 +0800

    add vit

--- event/early_exit.py
+++ event/early_exit.py
@@ -96,9 +96,8 @@ def train(model, train_dataset, test_dataset):
         acc = np.mean(acc, axis=0, where=acc >= 0)
         print('epoch', epoch)
         print('accuracy for early-exits:', acc)
-        if acc[-1].item() > best_acc:
-            best_acc = acc[-1].item()
-            torch.save(model.state_dict(), str(epoch) + '_' + str(acc[-1].item()) + '.pth')
+        # best_acc = acc[-1].item()
+        torch.save(model.state_dict(), str(epoch) + '_' + str(acc[-1].item()) + '.pth')
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(1)
