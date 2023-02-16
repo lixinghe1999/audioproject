8c201a1d854a713fd66b234b920e6c9ee67a21f5
lixinghe1999 <helixing99@gmail.com>
2023-02-16 19:48:56 +0800

    add vit

--- event/early_exit.py
+++ event/early_exit.py
@@ -13,7 +13,7 @@ def profile(model, test_dataset):
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, num_workers=1, batch_size=1, shuffle=False)
     model.eval()
     model.exit = True
-    thresholds = [0.8, 0.9, 0.95, 0.99]
+    thresholds = [0.7, 0.8, 0.9, 0.95, 0.99]
     with torch.no_grad():
         for threshold in thresholds:
             acc = []
