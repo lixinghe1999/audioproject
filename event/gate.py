8c1dbec528b49eabf49525fe35436209e26e494a
lixinghe1999 <helixing99@gmail.com>
2023-02-21 14:24:48 +0800

    add gate network

--- event/gate.py
+++ event/gate.py
@@ -23,7 +23,7 @@ def profile(model, test_dataset):
             gate_label = model.label(output_cache, text)
             gate_label = torch.argmax(gate_label, dim=-1, keepdim=True).cpu().numpy()
             print(np.argmax(output.cpu().numpy(), axis=-1), text, error)
-            if np.argmax(output.cpu().numpy(), axis=-1) != text:
+            if np.argmax(output.cpu().numpy(), axis=-1) != text.item():
                 error += 1
             print(error)
             compress_level.append(gate_label)
