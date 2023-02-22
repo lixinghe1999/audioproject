4750e935f6e5bbcbb5eafab0841048f8b46f3063
lixinghe1999 <helixing99@gmail.com>
2023-02-22 20:51:04 +0800

    add gate network

--- event/vanilla.py
+++ event/vanilla.py
@@ -33,9 +33,9 @@ def update_lr(optimizer, multiplier = .1):
         param_group['lr'] = param_group['lr'] * multiplier
     optimizer.load_state_dict(state_dict)
 def train(model, train_dataset, test_dataset):
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, num_workers=4, batch_size=8, shuffle=True,
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, num_workers=4, batch_size=24, shuffle=True,
                                                drop_last=True, pin_memory=False)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, num_workers=4, batch_size=8, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, num_workers=4, batch_size=24, shuffle=False)
     best_acc = 0
     for param in model.audio.parameters():
         param.requires_grad = False
