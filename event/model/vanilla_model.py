bccf8c73ac1340eb68599f565a28da29122e973a
lixinghe1999 <helixing99@gmail.com>
2023-02-22 20:23:28 +0800

    add gate network

--- event/model/vanilla_model.py
+++ event/model/vanilla_model.py
@@ -82,7 +82,7 @@ class AVnet(nn.Module):
         self.original_embedding_dim = self.audio.v.pos_embed.shape[2]
         self.bottleneck_token = nn.Parameter(torch.zeros(1, 4, self.original_embedding_dim))
         self.fusion_stage = 6
-        self.bottleneck = nn.ModuleList([EncoderLayer(self.original_embedding_dim, 2048, 4, 0.1) for _ in range(12-self.fusion_stage)])
+        self.bottleneck = nn.ModuleList([EncoderLayer(self.original_embedding_dim, 512, 4, 0.1) for _ in range(12-self.fusion_stage)])
         self.projection = nn.Sequential(nn.LayerNorm(self.original_embedding_dim),
                                       nn.Linear(self.original_embedding_dim, 309))
     @autocast()
