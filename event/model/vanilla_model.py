78d31aefbd5953cb398cffab561d943af430c2ca
lixinghe1999 <helixing99@gmail.com>
2023-02-22 20:21:36 +0800

    add gate network

--- event/model/vanilla_model.py
+++ event/model/vanilla_model.py
@@ -35,16 +35,11 @@ class BottleNeck_attention(nn.Module):
         # 3. do scale dot product to compute similarity
         # out, attention = self.attention(q, k, v, mask=mask)
         batch_size, length, d_tensor = q.size()
-
         # 1. dot product Query with Key^T to compute similarity
         k_t = k.transpose(1, 2)
-        print(k.shape, k_t.shape)
         score = (q[:, :self.bottleneck, :] @ k_t) / d_tensor**0.5  # scaled dot product
-        print(score.shape)
         score = nn.functional.softmax(score)
-
         v = score @ v
-        print(v.shape)
         return v
 
 
@@ -88,8 +83,8 @@ class AVnet(nn.Module):
         self.bottleneck_token = nn.Parameter(torch.zeros(1, 4, self.original_embedding_dim))
         self.fusion_stage = 6
         self.bottleneck = nn.ModuleList([EncoderLayer(self.original_embedding_dim, 2048, 4, 0.1) for _ in range(12-self.fusion_stage)])
-        self.projection = nn.Sequential(nn.LayerNorm(self.original_embedding_dim * 2),
-                                      nn.Linear(self.original_embedding_dim * 2, 309))
+        self.projection = nn.Sequential(nn.LayerNorm(self.original_embedding_dim),
+                                      nn.Linear(self.original_embedding_dim, 309))
     @autocast()
     def forward(self, audio, image):
         B = audio.shape[0]
@@ -118,63 +113,13 @@ class AVnet(nn.Module):
             if i >= self.fusion_stage:
                 bottleneck_token = self.bottleneck[i-self.fusion_stage](torch.cat((bottleneck_token, audio, image), dim=1))
 
-        audio = self.audio.v.norm(audio)
-        audio = (audio[:, 0] + audio[:, 1]) / 2
-        image = self.image.v.norm(image)
-        image = (image[:, 0] + image[:, 1]) / 2
-
-        output = self.projection(torch.cat([audio, image], dim=-1))
+        output = self.projection(torch.mean(bottleneck_token, dim=1))
+        # audio = self.audio.v.norm(audio)
+        # audio = (audio[:, 0] + audio[:, 1]) / 2
+        # image = self.image.v.norm(image)
+        # image = (image[:, 0] + image[:, 1]) / 2
+        # output = self.projection(torch.cat([audio, image], dim=-1))
         return output
-class SingleNet(nn.Module):
-    def __init__(self, modality='A', num_cls=309):
-        '''
-        :param modality: A or V
-        :param layers: resnet18: [2, 2, 2, 2] resnet34: [3, 4, 6, 3]
-        :param num_cls: number of class
-        '''
-        super(SingleNet, self).__init__()
-        self.modality = modality
-        if modality == 'A':
-            self.net = ResNet(img_channels=1, layers=(3, 4, 6, 3), block=BasicBlock, num_classes=num_cls)
-            self.n_fft = 512
-            self.hop_length = 512
-            self.win_length = 512
-            self.spec_scale = 224
-            self.normalized = True
-            self.onesided = True
-        else:
-            self.net = ResNet(img_channels=3, layers=(3, 4, 6, 3), block=BasicBlock, num_classes=1000)
-            self.net.load_state_dict(torch.load('resnet34.pth'))
-            self.net.fc = torch.nn.Linear(512, num_cls)
-    def preprocessing_audio(self, audio):
-        spec = torch.stft(audio.squeeze(1), n_fft=self.n_fft, hop_length=self.hop_length,
-                          win_length=self.win_length, window=torch.hann_window(self.win_length, device=audio.device),
-                          pad_mode='reflect', normalized=self.normalized, onesided=True, return_complex=True)
-        spec = torch.abs(spec)
-        spec = torch.log(spec + 1e-7)
-        mean = torch.mean(spec)
-        std = torch.std(spec)
-        spec = (spec - mean) / (std + 1e-9)
-        spec = torch.nn.functional.interpolate(spec.unsqueeze(1), size=self.spec_scale, mode='bilinear')
-        return spec
-    def forward(self, data):
-        if self.modality == 'A':
-            data = self.preprocessing_audio(data)
-
-        data = self.net.conv1(data)
-        data = self.net.bn1(data)
-        data = self.net.relu(data)
-        data = self.net.maxpool(data)
-
-        data = self.net.layer1(data)
-        data = self.net.layer2(data)
-        data = self.net.layer3(data)
-        data = self.net.layer4(data)
-
-        data = self.net.avgpool(data)
-        data = torch.flatten(data, 1)
-        data = self.net.fc(data)
-        return data
 if __name__ == "__main__":
     num_cls = 100
     model = AVnet()
