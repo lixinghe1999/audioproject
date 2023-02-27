85ac7747a5451f2b2b222c661e03e2d403491f49
lixinghe1999 <helixing99@gmail.com>
2023-02-27 19:54:04 +0800

    add gate network

--- event/gate.py
+++ event/gate.py
@@ -82,6 +82,7 @@ def profile(model, test_dataset):
             output_cache, output = model(audio.to(device), image.to(device), 'no_exit')
 
             gate_label = model.label(output_cache, text)
+            print(gate_label)
             gate_label = torch.argmax(gate_label, dim=-1).cpu()
             print(gate_label)
             if torch.argmax(output).cpu() == text:
