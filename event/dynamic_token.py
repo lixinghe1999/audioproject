3da85f64a23b354b928a34648bf8a90948caeb13
lixinghe1999 <helixing99@gmail.com>
2023-03-06 18:39:03 +0800

    add gate network

--- event/dynamic_token.py
+++ event/dynamic_token.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(1)
     model = AVnet_Dynamic(pruning_loc=(), pretrained=True).to(device)
-    # model.image.load_state_dict(torch.load('assets/deit_base_patch16_224.pth')['model'], strict=False)
+    model.load_state_dict(torch.load('train_6_0.6778193269041527.pth'), strict=False)
 
     dataset = VGGSound()
     len_train = int(len(dataset) * 0.8)
