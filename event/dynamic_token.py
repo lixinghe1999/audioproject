079c8f9dac285f92fc9479572b82938c6ce49957
lixinghe1999 <helixing99@gmail.com>
2023-03-06 19:16:45 +0800

    add gate network

--- event/dynamic_token.py
+++ event/dynamic_token.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
         teacher_model.load_state_dict(torch.load('train_6_0.6778193269041527.pth'), strict=False)
         teacher_model.eval()
         criteria = DistillDiffPruningLoss_dynamic(teacher_model, torch.nn.CrossEntropyLoss(), clf_weight=1.0, keep_ratio=token_ratio,
-                                                  mse_token=True, ratio_weight=args.ratio_weight, distill_weight=0.5)
+                                                  mse_token=True, ratio_weight=2.0, distill_weight=0.5)
         train(model, train_dataset, test_dataset)
     elif args.task == 'test':
         test(model, test_dataset)
