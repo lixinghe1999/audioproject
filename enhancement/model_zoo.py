7e15f1fff925f5734b9bdebf6cb829abdfb3b0ff
lixinghe1999 <helixing99@gmail.com>
2023-03-08 09:27:29 +0800

    add gate network

--- enhancement/model_zoo.py
+++ enhancement/model_zoo.py
@@ -170,11 +170,10 @@ def train_fullsubnet(model, acc, noise, clean, optimizer, device='cuda'):
     _, _, clean_real, clean_imag = stft(clean, 512, 256, 512)
     cIRM = build_complex_ideal_ratio_mask(noisy_real.unsqueeze(1), noisy_imag.unsqueeze(1),
                                           clean_real.unsqueeze(1), clean_imag.unsqueeze(1))  # [B, F, T, 2]
-    # cIRM = drop_band(cIRM, 2)
+    cIRM = cIRM.squeeze(1).permute((0, 3, 1, 2))
 
     noisy_mag = noisy_mag.unsqueeze(1)
     cRM = model(noisy_mag)
-    print(cRM.shape, cIRM.shape)
     loss = F.mse_loss(cIRM, cRM)
 
     loss.backward()
