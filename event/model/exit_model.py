13a9ca68f36215ffaa331d2961893643f67a8084
lixinghe1999 <helixing99@gmail.com>
2023-03-09 15:27:08 +0800

    update early-exit-> concise!

--- event/model/exit_model.py
+++ event/model/exit_model.py
@@ -24,7 +24,7 @@ class AVnet_Exit(nn.Module):
         if pretrained:
             self.image.load_state_dict(torch.load('assets/deit_base_patch16_224.pth')['model'], strict=False)
         self.projection = nn.ModuleList([nn.Sequential(nn.LayerNorm(embed_dim*2), nn.Linear(embed_dim*2, 309))
-                                         for _ in range(1)])
+                                         for _ in range(12)])
         # self.projection = nn.Sequential(nn.LayerNorm(embed_dim * 2),
         #                           nn.Linear(embed_dim * 2, 309))
     def get_parameters(self):
@@ -45,6 +45,6 @@ class AVnet_Exit(nn.Module):
         # features = torch.cat([audio[:, 1:], image[:, 1:]], dim=1)
         x = torch.cat([audio[:, 0], image[:, 0]], dim=1)
         x = torch.flatten(x, start_dim=1)
-        output.append(self.projection[0](x))
+        output.append(self.projection[-1](x))
         return output
 
