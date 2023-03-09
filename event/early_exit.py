dacca4f78ca86afc2169dfc64945f8b56992028e
lixinghe1999 <helixing99@gmail.com>
2023-03-09 15:20:46 +0800

    update early-exit-> concise!

--- event/early_exit.py
+++ event/early_exit.py
@@ -50,8 +50,9 @@ def train_step(model, input_data, optimizer, criteria, label):
     outputs = model(audio, image)
     optimizer.zero_grad()
     loss = 0
-    for i, output in enumerate(outputs):
-        loss += (i+1)/12 * criteria(output, label)
+    # for i, output in enumerate(outputs):
+    #     loss += (i+1)/12 * criteria(output, label)
+    loss = criteria(outputs[-1], label)
     print(loss.item())
     loss.backward()
     optimizer.step()
