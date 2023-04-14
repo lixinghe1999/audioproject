589b2bdba9d4e903c97c95807c5e0db4a1906a50
lixinghe1999 <helixing99@gmail.com>
2023-04-14 10:54:22 +0800

    last-order

--- enhancement/feature.py
+++ enhancement/feature.py
@@ -85,7 +85,7 @@ def mag_phase(complex_tensor):
 
 def norm_amplitude(y, scalar=None, eps=1e-6):
     if not scalar:
-        scalar = y.abs().max() + eps
+        scalar = np.max(np.abs(y)) + eps
 
     return y / scalar, scalar
 
