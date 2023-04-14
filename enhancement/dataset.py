18cc9ab9d37f603c7d863c3d2a92e67d547779a2
lixinghe1999 <helixing99@gmail.com>
2023-04-14 09:17:32 +0800

    last-order

--- enhancement/dataset.py
+++ enhancement/dataset.py
@@ -153,7 +153,7 @@ class BaseDataset:
             else:
                 t_start = time.time()
                 # data, sr = librosa.load(file, sr=None)
-                data, sr = sf.read(file)
+                data, sr = sf.read(file, frames=duration, start=offset)
                 # data, sr = ta.load(file)
                 print(time.time() - t_start)
             return data, file
